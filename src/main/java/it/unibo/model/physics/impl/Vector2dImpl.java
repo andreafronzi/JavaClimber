fb64a6de66bd3860afb9c79721bf5e134c0e4eaf
AndreaFronzi <andrea.fronzi@studio.unibo.it>
2026-01-16 13:10:08 +0100

    Add JavaDoc comments for `Vector2dImpl` class, fields, and constructor

--- src/main/java/it/unibo/model/physics/impl/Vector2dImpl.java
+++ src/main/java/it/unibo/model/physics/impl/Vector2dImpl.java
@@ -2,11 +2,27 @@ package it.unibo.model.physics.impl;
 
 import it.unibo.model.physics.api.Vector2d;
 
+/**
+ * Implementation of a two-dimensional vector (Cartesian Coordinates) which can be used to rapresent either Position or Speed.
+ */
 public class Vector2dImpl implements Vector2d {
-    private double x;
-    private double y;
 
-    public Vector2dImpl(final double x, final double y) {
+  /**
+   * The x-coordinate of the vector.
+   */
+  private double x;
+  /**
+   * The y-coordinate of the vector.
+   */
+  private double y;
+
+  /**
+   * Constructs a two-dimensional vector with the specified x and y coordinates.
+   *
+   * @param x the x-coordinate of the vector
+   * @param y the y-coordinate of the vector
+   */
+  public Vector2dImpl(final double x, final double y) {
         this.x = x;
         this.y = y;
     }
