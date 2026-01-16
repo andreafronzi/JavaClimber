52fc5d3f2cdab724fbc3e12d92870c7878e97052
AndreaFronzi <andrea.fronzi@studio.unibo.it>
2026-01-16 13:34:22 +0100

    Update `GameObj` to use `Vector2d` instead of `Position` and add comprehensive JavaDoc comments to class and methods. Modified position's getter and add position's setter

--- src/main/java/it/unibo/model/gameObj/api/GameObj.java
+++ src/main/java/it/unibo/model/gameObj/api/GameObj.java
@@ -1,20 +1,53 @@
 package it.unibo.model.gameObj.api;
 
-import it.unibo.model.physics.api.Position;
+import it.unibo.model.physics.api.Vector2d;
 
+/**
+ * Abstract game object with a two-dimensional position.
+ * This class provides common functionalities to manage the position
+ * of an object in a Cartesian coordinate system.
+ */
 public abstract class GameObj {
-  private Position position;
 
-  public GameObj(final Position position) {
+  /**
+   * The two-dimensional position of the object.
+   */
+  private final Vector2d position;
+
+  /**
+   * Constructs a new GameObj with the specified two-dimensional position.
+   *
+   * @param position the initial position of the game object,
+   *                 provided as a {@link Vector2d} instance
+   */
+  public GameObj(final Vector2d position) {
     this.position = position;
   }
 
-  public Position getPosition() {
-    return position;
+  /**
+   *
+   * @return x-coordinate of the position of the object
+   */
+  public double getPosX() {
+    return this.position.getX();
   }
 
-  public void setPosition(final Position position) {
-    this.position = position;
+  /**
+   *
+   * @return y-coordinate of the position of the object
+   */
+  public double getPosY() {
+    return this.position.getY();
+  }
+
+  /**
+   * Set the Position of the object with the one provided.
+   *
+   * @param position the new position of the object
+   */
+  public void setPosition(final Vector2d position) {
+    this.position.setX(position.getX());
+    this.position.setY(position.getY());
   }
 
 }
