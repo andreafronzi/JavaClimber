9cf9d541633121ebdc15bff6a9e39be62a01138f
AndreaFronzi <andrea.fronzi@studio.unibo.it>
2026-01-18 02:21:23 +0100

    Add `AlienPhysic` interface with `update` method and JavaDoc comments

---
+++ src/main/java/it/unibo/model/physics/api/AlienPhysic.java
@@ -0,0 +1,18 @@
+package it.unibo.model.physics.api;
+
+import it.unibo.model.gameObj.api.Alien;
+import it.unibo.model.gameObj.impl.Boundary;
+
+/**
+ * Rappresent the application of the chosen physic over the Alien.
+ */
+public interface AlienPhysic {
+
+  /**
+   * Upply chosen physic effect to alien's speed
+   * @param alien the alien to update
+   * @param dt the time step
+   * @param boundary the boundary
+   */
+  void update(Alien alien, double dt, Boundary boundary);
+}
