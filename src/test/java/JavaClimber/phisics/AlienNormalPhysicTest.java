82f4bb4137db4e705596fc2b3c096fa0170cab12
AndreaFronzi <andrea.fronzi@studio.unibo.it>
2026-01-18 02:22:53 +0100

    Add `AlienNormalPhysicTest` to verify alien movement mechanics and boundary pacman effects

---
+++ src/test/java/JavaClimber/phisics/AlienNormalPhysicTest.java
@@ -0,0 +1,78 @@
+package JavaClimber.phisics;
+
+import it.unibo.model.gameObj.api.Alien;
+import it.unibo.model.gameObj.impl.AlienImpl;
+import it.unibo.model.gameObj.impl.Boundary;
+import it.unibo.model.physics.api.AlienPhysic;
+import it.unibo.model.physics.impl.AlienNormalPhysic;
+import it.unibo.model.physics.impl.Vector2dImpl;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AlienNormalPhysicTest {
+
+  private static final double EPSILON = 0.001;
+
+  private static final double X = 10;
+  private static final double Y = 100;
+
+  private static final double X1 = 90;
+
+  private static final double SPEED_X = 0;
+  private static final double SPEED_Y = 0;
+
+  private static final double SPEED1_X = 50;
+  private static final double SPEED2_X = -200;
+
+  private static final double WIDTH = 50;
+  private static final double HEIGTH = 50;
+
+  private static final double LEFT_BOUNDARY = 0;
+  private static final double RIGHT_BOUNDARY = 100;
+
+  private static final double DT = 0.40;
+
+  private static final double NEW_SPEED_Y = 20;
+  private static final double NEW_Y = 108;
+
+  /**
+   * Test the {@link AlienNormalPhysic#update(Alien, double, Boundary)} method to verify expected vertical movement behavior.
+   */
+  @Test
+  void testUpdateAlienPosition() {
+    final AlienPhysic physic = new AlienNormalPhysic();
+    final Alien alien = new AlienImpl(new Vector2dImpl(X, Y), new Vector2dImpl(SPEED_X, SPEED_Y), WIDTH, HEIGTH);
+    assertEquals(0,alien.getSpeedY(), EPSILON);
+    final Boundary boundary = new Boundary(LEFT_BOUNDARY, RIGHT_BOUNDARY);
+    physic.update(alien, DT, boundary);
+    assertEquals(NEW_SPEED_Y, alien.getSpeedY(), EPSILON);
+    assertEquals(NEW_Y, alien.getPosY(), EPSILON);
+  }
+
+  /**
+   * Tests the behavior of the {@link AlienNormalPhysic#update(Alien, double, Boundary)} method.
+   * It verifies that Pacman effect correctly repositions the alien to the left edge of the boundary.
+   */
+  @Test
+  void testRightToLeftPacmanEffect() {
+    final AlienPhysic physic = new AlienNormalPhysic();
+    final Alien alien = new AlienImpl(new Vector2dImpl(X1, Y), new Vector2dImpl(SPEED1_X, SPEED_Y), WIDTH, HEIGTH);
+    final Boundary boundary = new Boundary(LEFT_BOUNDARY, RIGHT_BOUNDARY);
+    physic.update(alien, DT, boundary);
+    assertEquals(LEFT_BOUNDARY, alien.getPosX(), EPSILON);
+  }
+
+  /**
+   * Tests the behavior of the {@link AlienNormalPhysic#update(Alien, double, Boundary)} method.
+   * It verifies that Pacman effect correctly repositions the alien to the right edge of the boundary.
+   */
+  @Test
+  void testLeftToRightPacmanEffect() {
+    final AlienPhysic physic = new AlienNormalPhysic();
+    final Alien alien = new AlienImpl(new Vector2dImpl(X, Y), new Vector2dImpl(SPEED2_X, SPEED_Y), WIDTH, HEIGTH);
+    final Boundary boundary = new Boundary(LEFT_BOUNDARY, RIGHT_BOUNDARY);
+    physic.update(alien, DT, boundary);
+    assertEquals(RIGHT_BOUNDARY - WIDTH, alien.getPosX(), EPSILON);
+  }
+}
