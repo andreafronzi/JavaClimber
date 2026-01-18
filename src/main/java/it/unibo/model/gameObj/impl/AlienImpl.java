48f01a60a68696f5c554af204569eb35126d2a76
AndreaFronzi <andrea.fronzi@studio.unibo.it>
2026-01-18 01:53:35 +0100

    Make `AlienImpl` constructor accept initial speed instead of defaulting to null speed.

--- src/main/java/it/unibo/model/gameObj/impl/AlienImpl.java
+++ src/main/java/it/unibo/model/gameObj/impl/AlienImpl.java
@@ -3,12 +3,9 @@ package it.unibo.model.gameObj.impl;
 import it.unibo.model.gameObj.api.Alien;
 import it.unibo.model.gameObj.api.GameObj;
 import it.unibo.model.physics.api.Vector2d;
-import it.unibo.model.physics.impl.Vector2dImpl;
 
 
 public class AlienImpl extends GameObj implements Alien {
-
-  private static final double INITIAL_SPEED_COORDINATE = 0;
 
   /**
    * The two-dimensional speed of the Alien.
@@ -21,9 +18,9 @@ public class AlienImpl extends GameObj implements Alien {
   /**
    * Constructs a new Alien with the specified two-dimensional position, null speed, and specified width and heigth.
    */
-  public AlienImpl(final Vector2d position, final double width, final double heigth) {
+  public AlienImpl(final Vector2d position, final Vector2d speed, final double width, final double heigth) {
     super(position);
-    this.speed = new Vector2dImpl(INITIAL_SPEED_COORDINATE, INITIAL_SPEED_COORDINATE);
+    this.speed = speed;
     this.width = width;
     this.heigth = heigth;
 
