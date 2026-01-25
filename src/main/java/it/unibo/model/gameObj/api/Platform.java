802d96d5a2aaa049ed78fdc6963370289aab4119
AndreaFronzi <andrea.fronzi@studio.unibo.it>
2026-01-25 18:49:26 +0100

    Add JavaDoc comments and enhance `Platform` interface refining the idea: Platform should'n be a god class which contains enemy, and coins

--- src/main/java/it/unibo/model/gameObj/api/Platform.java
+++ src/main/java/it/unibo/model/gameObj/api/Platform.java
@@ -1,20 +1,40 @@
 package it.unibo.model.gameObj.api;
 
+import it.unibo.model.gameObj.impl.Boundary;
+import it.unibo.model.physics.api.Vector2d;
+
+
+/**
+ * Represents an Alien entity in a two-dimensional game environment.
+ */
 public interface Platform {
 
-  /** */
-  void updatePosition();
-
+  /**
+   * This method adds a Movement behaviour to the Platform.
+   */
   void addMovementBehaviour();
-  void addJumpBehaviour();
-  void addBreakBehaviour();
 
-  void removeEnemy();
-  void removeCoin();
-  void removeGadget();
+  /**
+   * This method adds the behavior the platform has when touched
+   */
+  void addOnTouchBehaviour();
 
-  boolean containsEnemy();
-  boolean containsCoin();
+  /**
+   *
+   * @return whether the platform contains a gadget or not.
+   */
   boolean containsGadget();
 
+  /**
+   * Platform's touch reaction.
+   */
+  void onTouch();
+
+  /**
+   * Update Platform's position. Computed position deals with elapsed time between two updates.
+   * @param position Platform's position to update
+   * @param dt  the time interval between two updates
+   * @param boundary the boundary of the world
+   */
+  void updatePosition(Vector2d position, double dt, Boundary boundary);
 }
