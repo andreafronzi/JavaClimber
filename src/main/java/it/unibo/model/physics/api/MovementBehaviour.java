fd86fff5bb194cac56ab32c62f348f058a5ba189
AndreaFronzi <andrea.fronzi@studio.unibo.it>
2026-01-26 17:36:47 +0100

    Add `MovementBehaviour` interface with `updatePosition` method and JavaDoc comments

---
+++ src/main/java/it/unibo/model/physics/api/MovementBehaviour.java
@@ -0,0 +1,19 @@
+package it.unibo.model.physics.api;
+
+import it.unibo.model.gameObj.impl.Boundary;
+
+/**
+ * Interface representing game obgect's movement behavior.
+ */
+public interface MovementBehaviour {
+
+  /**
+   * Update Obj's position. Computed position deals with elapsed time between two updates.
+   * @param position Obj's position to update
+   * @param width Obj's width
+   * @param heigth Obj's heigth
+   * @param dt the time interval between two updates
+   * @param boundary the boundary of the world
+   */
+  void updatePosition(Vector2d position, double width, double heigth, double dt, Boundary boundary);
+}
