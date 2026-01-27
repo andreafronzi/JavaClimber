850f6cfa1868939b49a46a47f50e15ac06878da3
AndreaFronzi <andrea.fronzi@studio.unibo.it>
2026-01-27 17:33:54 +0100

    Refine `Platform` interface: remove unused methods and update `updatePosition` signature in order to deal with Interface Segregation principle.

--- src/main/java/it/unibo/model/gameObj/api/Platform.java
+++ src/main/java/it/unibo/model/gameObj/api/Platform.java
@@ -1,8 +1,6 @@
 package it.unibo.model.gameObj.api;
 
 import it.unibo.model.gameObj.impl.Boundary;
-import it.unibo.model.physics.api.Vector2d;
-
 
 /**
  * Represents an Alien entity in a two-dimensional game environment.
@@ -10,18 +8,8 @@ import it.unibo.model.physics.api.Vector2d;
 public interface Platform {
 
   /**
-   * This method adds a Movement behaviour to the Platform.
-   */
-  void addMovementBehaviour();
-
-  /**
-   * This method adds the behavior the platform has when touched
-   */
-  void addOnTouchBehaviour();
-
-  /**
    *
-   * @return whether the platform contains a gadget or not.
+   * @return { @code True } whether the platform contains a gadget or not.
    */
   boolean containsGadget();
 
@@ -32,9 +20,8 @@ public interface Platform {
 
   /**
    * Update Platform's position. Computed position deals with elapsed time between two updates.
-   * @param position Platform's position to update
    * @param dt  the time interval between two updates
    * @param boundary the boundary of the world
    */
-  void updatePosition(Vector2d position, double dt, Boundary boundary);
+  void updatePosition(double dt, Boundary boundary);
 }
