4ab5927738b023bc46734ebec0d2e8bb90b34d24
Pietro Siboni <pietro.siboni3@studio.unibo.it>
2026-01-28 13:25:45 +0100

    Make interface and implementation of ShopController

--- src/main/java/it/unibo/controller/api/ShopController.java
+++ src/main/java/it/unibo/controller/api/ShopController.java
@@ -1,5 +1,17 @@
 package it.unibo.controller.api;
 
+/**
+ * Interface 
+ */
 public interface ShopController {
 
+    void upgradeJump();
+
+    void upgradeSpeed();
+
+    void buyTemporaryItem(int index);
+
+    void buySkin(int index);
+
+    void exit();
 }
--- src/main/java/it/unibo/controller/impl/ShopControllerImpl.java
+++ src/main/java/it/unibo/controller/impl/ShopControllerImpl.java
@@ -1,5 +1,60 @@
 package it.unibo.controller.impl;
 
-public class ShopControllerImpl {
+import java.util.Comparator;
+import java.util.List;
+
+import it.unibo.controller.api.ShopController;
+import it.unibo.model.shop.api.ShopItem;
+import it.unibo.model.shop.api.ShopManager;
+
+public class ShopControllerImpl implements ShopController {
+
+    private final ShopManager shopManager;
+    
+
+    public ShopControllerImpl(final ShopManager shopManager) {
+        this.shopManager = shopManager;
+    }
+
+    @Override
+    public void upgradeJump() {
+        buyNextLevel("pp_jump");
+    }
+
+    @Override
+    public void upgradeSpeed() {
+        buyNextLevel("pp_speed");
+    }
+
+    @Override
+    public void buyTemporaryItem(int index) {
+        List<ShopItem> tempItems = shopManager.getTemporaryUpgrades();
+        if (index >= 0 && index < tempItems.size()) {
+            shopManager.buyItem(tempItems.get(index));
+        }
+    }
+
+    @Override
+    public void buySkin(int index) {
+        List<ShopItem> skins = shopManager.getSkins();
+        if (index >= 0 && index < skins.size()) {
+            shopManager.buyItem(skins.get(index));
+        }
+    }
+
+    @Override
+    public void exit() {
+        // TODO Auto-generated method stub(back to menu)
+        throw new UnsupportedOperationException("Unimplemented method 'exit'");
+    }
+
+    private void buyNextLevel(String prefix){
+        shopManager.getPermanentUpgrades().stream()
+                .filter(item -> item.getId().startsWith(prefix))
+                .filter(item -> !shopManager.isAlreadyOwned(item))
+                .sorted(Comparator.comparing(ShopItem::getId))
+                .findFirst()
+                .ifPresent(shopManager::buyItem);
+    }
     
 }
