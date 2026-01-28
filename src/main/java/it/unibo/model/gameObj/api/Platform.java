0afba976b893a11b08c6c88cd0ba7023e66a0e1d
AndreaFronzi <andrea.fronzi@studio.unibo.it>
2026-01-28 14:19:44 +0100

    Expand `Platform` interface: add position, size accessors, and boundary-aware touch reaction

--- src/main/java/it/unibo/model/gameObj/api/Platform.java
+++ src/main/java/it/unibo/model/gameObj/api/Platform.java
@@ -1,22 +1,55 @@
 package it.unibo.model.gameObj.api;
 
 import it.unibo.model.gameObj.impl.Boundary;
+import it.unibo.model.physics.api.Vector2d;
 
 /**
- * Represents an Alien entity in a two-dimensional game environment.
+ * Represents a Platform entity in a two-dimensional game environment.
  */
 public interface Platform {
 
   /**
    *
-   * @return { @code True } whether the platform contains a gadget or not.
+   * @return True whether the platform contains a gadget or not.
    */
   boolean containsGadget();
 
   /**
-   * Platform's touch reaction.
+   *
+   * @return x-coordinate of the position of the platform
    */
-  void onTouch();
+  double getPosX();
+
+  /**
+   *
+   * @return y-coordinate of the position of the platform
+   */
+  double getPosY();
+
+  /**
+   *
+   * @return height of the platform
+   */
+  double getHeight();
+
+  /**
+   * 
+   * @return width of the platform
+   */
+  double getWidth();
+
+  /**
+   * Platform's touch reaction.
+   * @param boundary the boundary of the world
+   */
+  void onTouch(final Boundary boundary);
+
+  /**
+   * Set the Position of the object with the one provided.
+   *
+   * @param position the new position of the object
+   */
+  public void setPosition(Vector2d position);
 
   /**
    * Update Platform's position. Computed position deals with elapsed time between two updates.
