96c3afe008a9f2ec0713dbf5d414c52f370b3927
Pietro Siboni <pietro.siboni3@studio.unibo.it>
2026-01-28 17:15:00 +0100

    Add methods with javadoc comments in InventoryController interface

--- src/main/java/it/unibo/controller/api/InventoryController.java
+++ src/main/java/it/unibo/controller/api/InventoryController.java
@@ -2,4 +2,40 @@ package it.unibo.controller.api;
 
 public interface InventoryController {
 
+    /**
+     * Select a skin from the list of skins.
+     * @param index index in the list of skins
+     */
+    void selectSkin(int index);
+
+    /**
+     * Increases the level of jump to the next.
+     */
+    void plusJump();
+
+    /**
+     * Decreases the level of jump to the previus.
+     */
+    void minusJump();
+
+    /**
+     * Increases the level of speed to the next.
+     */
+    void plusVelocity();
+
+    /**
+     * Decreases the level of speed to the previus.
+     */
+    void minusVelocity();
+
+    /**
+     * Activate/Disactivate a temporary power up.
+     * @param index index of temporary power ups in the inventory
+     */
+    void toggleTemporaryItem(int index);
+
+    /**
+     * Exit from the Inventory and back to the menù.
+     */
+    void exit();
 }
