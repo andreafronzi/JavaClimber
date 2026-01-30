d484d41dd0353a0720c0da10b538245e5fcd24a3
AndreaFronzi <andrea.fronzi@studio.unibo.it>
2026-01-30 17:09:39 +0100

    Refine `AlienImpl`: fix typo in `heigth` field and accessor, add `AlienPhysic` field, and implement `setPhysic` and `updatePosition` methods to deal with State Pattern

--- src/main/java/it/unibo/model/gameObj/impl/AlienImpl.java
+++ src/main/java/it/unibo/model/gameObj/impl/AlienImpl.java
@@ -2,27 +2,42 @@ package it.unibo.model.gameObj.impl;
 
 import it.unibo.model.gameObj.api.Alien;
 import it.unibo.model.gameObj.api.GameObj;
+import it.unibo.model.physics.api.AlienPhysic;
 import it.unibo.model.physics.api.Vector2d;
+import it.unibo.model.physics.impl.AlienNormalPhysic;
 
 
 public class AlienImpl extends GameObj implements Alien {
+
+  /**
+   * The width of the Alien.
+   */
+  private final double width;
+
+  /**
+   * The height of the Alien.
+   */
+  private final double height;
 
   /**
    * The two-dimensional speed of the Alien.
    */
   private final Vector2d speed;
 
-  private final double width;
-  private final double heigth;
+  /**
+   * The Alien's physic.
+   */
+  private AlienPhysic physic;
 
   /**
-   * Constructs a new Alien with the specified two-dimensional position, null speed, and specified width and heigth.
+   * Constructs a new Alien with the specified two-dimensional position, null speed, and specified width and height.
    */
-  public AlienImpl(final Vector2d position, final Vector2d speed, final double width, final double heigth) {
+  public AlienImpl(final Vector2d position, final Vector2d speed, final double width, final double height) {
     super(position);
     this.speed = speed;
     this.width = width;
-    this.heigth = heigth;
+    this.height = height;
+    this.physic = new AlienNormalPhysic();
 
   }
 
@@ -32,8 +47,8 @@ public class AlienImpl extends GameObj implements Alien {
   }
 
   @Override
-  public double getHeigth() {
-    return this.heigth;
+  public double getHeight() {
+    return this.height;
   }
 
   public double getSpeedX() {
@@ -45,8 +60,18 @@ public class AlienImpl extends GameObj implements Alien {
   }
 
   @Override
+  public void setPhysic(final AlienPhysic physic) {
+    this.physic = physic;
+  }
+
+  @Override
   public void setSpeed(final Vector2d speed) {
     this.speed.setX(speed.getX());
     this.speed.setY(speed.getY());
+  }
+
+  @Override
+  public void updatePosition(final double dt, final Boundary boundary) {
+    this.physic.update(this, dt, boundary);
   }
 }
