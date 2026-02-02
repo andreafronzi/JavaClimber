72dfb281a019a76e176bcb3dd1becd7b0bc4ba3f
marco <marcoaresu82@gmail.com>
2026-02-02 23:38:19 +0100

    created the pairImpl.java class and implemented it

---
+++ src/main/java/it/unibo/model/worldConstructor/impl/PairImpl.java
@@ -0,0 +1,28 @@
+package it.unibo.model.worldConstructor.impl;
+
+import java.util.function.Function;
+
+import it.unibo.model.physics.api.Vector2d;
+import it.unibo.model.worldConstructor.api.Pair;
+
+public class PairImpl<X> implements Pair<X>{
+
+    private final double chance;
+    private final Function<Vector2d,X> function;
+
+    public PairImpl(final double chance, Function<Vector2d,X> function){
+        this.chance = chance;
+        this.function = function;
+    }
+
+    @Override
+    public double getChance() {
+        return this.chance;
+    }
+
+    @Override
+    public X createGameObj(final Vector2d pos) {
+        return this.function.apply(pos);    
+    }
+    
+}
