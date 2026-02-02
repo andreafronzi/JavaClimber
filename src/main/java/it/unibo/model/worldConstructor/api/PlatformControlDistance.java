8d83e3234c956e5dc64612511c3f647148b58306
marco <marcoaresu82@gmail.com>
2026-02-02 21:14:35 +0100

    created the PlatformControlDistance and WorldConstructor interfaces

---
+++ src/main/java/it/unibo/model/worldConstructor/api/PlatformControlDistance.java
@@ -0,0 +1,11 @@
+package it.unibo.model.worldConstructor.api;
+
+import it.unibo.model.physics.api.Vector2d;
+
+public interface PlatformControlDistance {
+    
+    public Vector2d generatePosition();
+
+    public void setDifficult();
+
+}
---
+++ src/main/java/it/unibo/model/worldConstructor/api/WorldConstructor.java
@@ -0,0 +1,9 @@
+package it.unibo.model.worldConstructor.api;
+
+import it.unibo.model.gameObj.api.Platform;
+
+public interface WorldConstructor {
+
+    public Platform createPlatform();
+
+}
