a60ea5e5302c8ca172a198699abce5f31e43523b
marco <marcoaresu82@gmail.com>
2026-02-03 00:52:08 +0100

    created Difficult interface with methods to get distance and maximums

---
+++ src/main/java/it/unibo/model/worldConstructor/api/Difficult.java
@@ -0,0 +1,13 @@
+package it.unibo.model.worldConstructor.api;
+
+public interface Difficult {
+    
+    public double getDistanceY();
+
+    public double getDistanceX();
+
+    public int getMaxMoney();
+
+    public int getMaxMonster(); 
+
+}
