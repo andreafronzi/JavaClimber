ca7beb409f9e51001683690bcf6750c12a931198
AndreaFronzi <andrea.fronzi@studio.unibo.it>
2026-02-04 00:31:54 +0100

    Add EliCap class with Gadget behavior and effect on Alien physics

---
+++ src/main/java/it/unibo/model/gameObj/impl/EliCap.java
@@ -0,0 +1,23 @@
+package it.unibo.model.gameObj.impl;
+
+import it.unibo.model.gameObj.api.Alien;
+import it.unibo.model.gameObj.api.Gadget;
+import it.unibo.model.gameObj.api.GameObj;
+import it.unibo.model.physics.alienPhysic.impl.AlienEliCapPhysic;
+import it.unibo.model.physics.api.Vector2d;
+
+public class EliCap extends GameObj implements Gadget{
+
+  private static final double TIME_INTERVALL = 2;
+  private static final double VERTICAL_SPEED = 10;
+
+  public EliCap(final double height, final double width, final Vector2d position) {
+        super(height, width, position);
+    }
+
+    @Override
+    public void onCollect(final Alien alien) {
+        alien.setPhysic(new AlienEliCapPhysic(TIME_INTERVALL, VERTICAL_SPEED));
+    }
+
+}
