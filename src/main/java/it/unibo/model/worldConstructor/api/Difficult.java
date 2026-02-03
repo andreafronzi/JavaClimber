40b719f47992f7ce41fcc088a44c60bef609f5a3
marco <marcoaresu82@gmail.com>
2026-02-03 23:05:09 +0100

    Removed the Difficult interface and replaced it with a record that includes distanceY, distanceX, maxMoney, maxMonster, maxGadget, and maxTrap

--- src/main/java/it/unibo/model/worldConstructor/api/Difficult.java
+++ src/main/java/it/unibo/model/worldConstructor/api/Difficult.java
@@ -1,13 +1,4 @@
 package it.unibo.model.worldConstructor.api;
 
-public interface Difficult {
-    
-    public double getDistanceY();
-
-    public double getDistanceX();
-
-    public int getMaxMoney();
-
-    public int getMaxMonster(); 
-
+public record Difficult(double distanceY, double distanceX, int maxMoney, int maxMonster, int maxGadget, int maxTrap) {
 }
