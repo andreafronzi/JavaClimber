d25a0fec427686e958f42fc01a5c105369722ab7
marco <marcoaresu82@gmail.com>
2026-02-04 00:04:59 +0100

    add maxgadgets and maxtrap var in the record

--- src/main/java/it/unibo/model/worldConstructor/impl/Difficult.java
+++ src/main/java/it/unibo/model/worldConstructor/impl/Difficult.java
@@ -1,4 +1,4 @@
-package it.unibo.model.worldConstructor.api;
+package it.unibo.model.worldConstructor.impl;
 
 public record Difficult(double distanceY, double distanceX, int maxMoney, int maxMonster, int maxGadget, int maxTrap) {
 }
