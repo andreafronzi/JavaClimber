afce41de238250861a3d653c8014bbfa3d07e142
AndreaFronzi <andrea.fronzi@studio.unibo.it>
2026-02-04 00:06:09 +0100

    Add TemplatePhysic abstract class with template methods for alien physics update

---
+++ src/main/java/it/unibo/model/physics/alienPhysic/api/TemplatePhysic.java
@@ -0,0 +1,37 @@
+package it.unibo.model.physics.alienPhysic.api;
+
+import it.unibo.model.gameObj.api.Alien;
+import it.unibo.model.gameObj.impl.Boundary;
+import it.unibo.model.physics.impl.Vector2dImpl;
+
+public abstract class TemplatePhysic implements AlienPhysic {
+
+  @Override
+  public void update(final Alien alien, final double dt, final Boundary boundary) {
+    moveAlien(alien, dt);
+    verifyBoundaryTouch(alien, boundary);
+  }
+
+  /**
+   * Template Methods which update alien position and speed.
+   *
+   * @param alien the alien to update
+   * @param dt the time step
+   */
+  protected abstract void moveAlien(final Alien alien, final double dt);
+
+  /**
+   * Verify if the alien go beyond the boundary. Apply pacman effect if it happens.
+   *
+   * @param alien the alien which can go beyond the boundary
+   * @param boundary the boundaries, which can be crossed by the alien
+   */
+  private void verifyBoundaryTouch(final Alien alien, final Boundary boundary) {
+    if(alien.getPosX() + alien.getWidth() < boundary.x0()) {
+      alien.setPosition(new Vector2dImpl(boundary.x1() - alien.getWidth(), alien.getPosY()));
+    }
+    if(alien.getPosX() > boundary.x1()) {
+      alien.setPosition(new Vector2dImpl(boundary.x0(), alien.getPosY()));
+    }
+  }
+}
