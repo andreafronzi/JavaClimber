4e06068542302c66d08d7e56c57bf267d82c8ca2
AndreaFronzi <andrea.fronzi@studio.unibo.it>
2026-02-03 17:25:27 +0100

    Refactor `Platform` interface: extend `GameObject`, remove redundant methods, and update signatures

--- src/main/java/it/unibo/model/gameObj/api/Platform.java
+++ src/main/java/it/unibo/model/gameObj/api/Platform.java
@@ -6,51 +6,14 @@ import it.unibo.model.physics.api.Vector2d;
 /**
  * Represents a Platform entity in a two-dimensional game environment.
  */
-public interface Platform {
-
-  /**
-   *
-   * @return True whether the platform contains a gadget or not.
-   */
-  boolean containsGadget();
-
-  /**
-   *
-   * @return x-coordinate of the position of the platform
-   */
-  double getPosX();
-
-  /**
-   *
-   * @return y-coordinate of the position of the platform
-   */
-  double getPosY();
-
-  /**
-   *
-   * @return height of the platform
-   */
-  double getHeight();
-
-  /**
-   *
-   * @return width of the platform
-   */
-  double getWidth();
+public interface Platform extends GameObject{
 
   /**
    * Platform's touch reaction.
    *
    * @param boundary the boundary of the world
    */
-  void onTouch(final Boundary boundary);
-
-  /**
-   * Set the Position of the object with the one provided.
-   *
-   * @param position the new position of the object
-   */
-  public void setPosition(Vector2d position);
+  void onTouch(Boundary boundary);
 
   /**
    * Update Platform's position. Computed position deals with elapsed time between two updates.
