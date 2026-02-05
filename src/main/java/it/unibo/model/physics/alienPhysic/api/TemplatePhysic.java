36fd759ef60388798f41aedf293c8cb645b3c958
AndreaFronzi <andrea.fronzi@studio.unibo.it>
2026-02-05 14:33:09 +0100

    Enhance Javadoc in TemplatePhysic to document methods and clarify behavior

--- src/main/java/it/unibo/model/physics/alienPhysic/api/TemplatePhysic.java
+++ src/main/java/it/unibo/model/physics/alienPhysic/api/TemplatePhysic.java
@@ -4,8 +4,18 @@ import it.unibo.model.gameObj.api.Alien;
 import it.unibo.model.gameObj.impl.Boundary;
 import it.unibo.model.physics.impl.Vector2dImpl;
 
+/**
+ * Represents a template for the alien physic. It implements the update method, which is common for all the alien physic, and it defines an abstract method moveAlien, which is implemented by the different alien physic.
+ */
 public abstract class TemplatePhysic implements AlienPhysic {
 
+  /**
+   * Update alien position and speed. Verify if the alien go beyond the boundary and apply pacman effect if it happens.
+   * 
+   * @param alien the alien to update
+   * @param dt the time elapsed since the last update
+   * @param boundary the world's boundaries
+   */
   @Override
   public void update(final Alien alien, final double dt, final Boundary boundary) {
     moveAlien(alien, dt, boundary);
@@ -16,7 +26,7 @@ public abstract class TemplatePhysic implements AlienPhysic {
    * Template Methods which update alien position and speed.
    *
    * @param alien the alien to update
-   * @param dt the time step
+   * @param dt the time elapsed since the last update
    */
   protected abstract void moveAlien(final Alien alien, final double dt, final Boundary boundary);
 
