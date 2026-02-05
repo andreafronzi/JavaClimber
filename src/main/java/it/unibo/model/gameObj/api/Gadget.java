9a6e0283404d437acd348ee5a5b3ae889b774cff
AndreaFronzi <andrea.fronzi@studio.unibo.it>
2026-02-05 14:21:19 +0100

    Enhance documentation for Gadget interface to clarify its purpose and behavior

--- src/main/java/it/unibo/model/gameObj/api/Gadget.java
+++ src/main/java/it/unibo/model/gameObj/api/Gadget.java
@@ -1,5 +1,8 @@
 package it.unibo.model.gameObj.api;
 
+/**
+ * Represents a gadget, which can be collected by the {@link Alien}. When a gadget is collected change the behavior of the {@link Alien}.
+ */
 public interface Gadget extends GameObject{
 
   /**
