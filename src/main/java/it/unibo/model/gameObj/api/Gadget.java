66a2b865a6f8f16dd4d9c71b1f0f4861d83d4e4f
AndreaFronzi <andrea.fronzi@studio.unibo.it>
2026-02-05 17:07:05 +0100

    Update Gadget interface to extend StaticEntity instead of GameObject

--- src/main/java/it/unibo/model/gameObj/api/Gadget.java
+++ src/main/java/it/unibo/model/gameObj/api/Gadget.java
@@ -3,11 +3,11 @@ package it.unibo.model.gameObj.api;
 /**
  * Represents a gadget, which can be collected by the {@link Alien}. When a gadget is collected change the behavior of the {@link Alien}.
  */
-public interface Gadget extends GameObject{
+public interface Gadget extends StaticEntity{
 
   /**
    * When a gadget is collected change the behavior of the {@link Alien}.
-   * 
+   *
    * @param alien the alien that collects the gadget
    */
   void onCollect(Alien alien);
