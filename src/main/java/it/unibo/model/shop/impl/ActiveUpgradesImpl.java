b9ea973adaf05f8179e5cac7959c941ac00213c2
Pietro Siboni <pietro.siboni3@studio.unibo.it>
2026-02-09 23:52:41 +0100

    Update ActiveUpgrades for every type of item and create relative tests
    Also changed the description and stat in factory for coin temporary upgrades

--- src/main/java/it/unibo/model/shop/impl/ActiveUpgradesImpl.java
+++ src/main/java/it/unibo/model/shop/impl/ActiveUpgradesImpl.java
@@ -6,6 +6,8 @@ import it.unibo.model.shop.api.ShopItemFactory;
 
 public class ActiveUpgradesImpl implements ActiveUpgrades {
 
+    private static String PREFIX_SPEED = "pp_speed_";
+    private static String PREFIX_JUMP = "pp_jump_";
     private final Inventory inventory;
     private final ShopItemFactory factory;
     private double currentSpeedMultiplier;
@@ -25,24 +27,42 @@ public class ActiveUpgradesImpl implements ActiveUpgrades {
         this.currentCoinMultiplier = 1;
 
         for (String itemId : inventory.getActiveConsumables()) {
-            factory.getItemById(itemId).ifPresent(item -> {
-                item.getStats().forEach((stat, value) -> {
-                    switch (stat) {
-                        case SPEED:
-                            this.currentSpeedMultiplier = Math.max(this.currentSpeedMultiplier, value);
-                            break;
-                        case JUMP_HEIGHT:
-                            this.currentJumpMultiplier = Math.max(this.currentJumpMultiplier, value);
-                            break;
-                        case COIN_MULTIPLIER:
-                            this.currentCoinMultiplier = Math.max(this.currentCoinMultiplier, value.intValue());
-                            break;
-                        default:
-                            break;
-                    }
-                });
-            });
+            applyItemStats(itemId);
         }
+        
+        applyItemStats(inventory.getSelectedSkin());
+
+        int speedLevel = inventory.getSelectedSpeedLevel();
+        if (speedLevel > 0) {
+            String id = PREFIX_SPEED + speedLevel;
+            applyItemStats(id);
+        }
+
+        int jumpLevel = inventory.getSelectedJumpLevel();
+        if (jumpLevel > 0) {
+            String id = PREFIX_JUMP + jumpLevel;
+            applyItemStats(id);
+        }
+    }
+
+    private void applyItemStats(String itemId) {
+        factory.getItemById(itemId).ifPresent(item -> {
+            item.getStats().forEach((stat, value) -> {
+                switch (stat) {
+                    case SPEED:
+                        this.currentSpeedMultiplier = Math.max(this.currentSpeedMultiplier, value);
+                        break;
+                    case JUMP_HEIGHT:
+                        this.currentJumpMultiplier = Math.max(this.currentJumpMultiplier, value);
+                        break;
+                    case COIN_MULTIPLIER:
+                        this.currentCoinMultiplier = Math.max(this.currentCoinMultiplier, value.intValue());
+                        break;
+                    default:
+                        break;
+                }
+            });
+        });
     }
 
     @Override
