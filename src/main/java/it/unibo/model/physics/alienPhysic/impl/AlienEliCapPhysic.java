a13879044e2d1f71b57556f18bc35ade0d8b95b1
AndreaFronzi <andrea.fronzi@studio.unibo.it>
2026-02-09 19:23:18 +0100

    Enhance AlienEliCapPhysic to incorporate ActiveUpgrades for dynamic movement adjustments

--- src/main/java/it/unibo/model/physics/alienPhysic/impl/AlienEliCapPhysic.java
+++ src/main/java/it/unibo/model/physics/alienPhysic/impl/AlienEliCapPhysic.java
@@ -9,6 +9,7 @@ import it.unibo.model.gameObj.impl.Boundary;
 import it.unibo.model.physics.alienPhysic.api.AlienPhysic;
 import it.unibo.model.physics.alienPhysic.api.TemplatePhysic;
 import it.unibo.model.physics.impl.Vector2dImpl;
+import it.unibo.model.shop.api.ActiveUpgrades;
 
 /**
  * Represents the alien physic when the alien collects the EliCap gadget. The alien will have a vertical speed for a certain time interval, then it will return to normal physic.
@@ -40,18 +41,20 @@ public class AlienEliCapPhysic extends TemplatePhysic implements AlienPhysic {
    *
    * @param alien the alien to update
    * @param dt the time step
+   * @param boundary the boundary of the world
+   * @param activeUpgrades the active upgrades affecting the Alien
    */
   @Override
-  protected void moveAlien(final Alien alien, final double dt,  final Boundary boundary) {
+  protected void moveAlien(final Alien alien, final double dt,  final Boundary boundary, final ActiveUpgrades activeUpgrades) {
     alien.setSpeed(new Vector2dImpl(alien.getSpeedX(), this.verticalSpeed));
     if (this.timeInterval - dt >= 0) {
-      alien.setPosition(new Vector2dImpl(alien.getPosX() + alien.getSpeedX() * dt, alien.getPosY() + alien.getSpeedY() * dt));
+      alien.setPosition(new Vector2dImpl(alien.getPosX() + alien.getSpeedX() * dt * activeUpgrades.getSpeedMultiplier(), alien.getPosY() + alien.getSpeedY() * dt * activeUpgrades.getSpeedMultiplier()));
       this.timeInterval -= dt;
     } else {
       final double remainingDt = dt - this.timeInterval;
-      alien.setPosition(new Vector2dImpl(alien.getPosX() + alien.getSpeedX() * this.timeInterval, alien.getPosY() + alien.getSpeedY() * this.timeInterval));
+      alien.setPosition(new Vector2dImpl(alien.getPosX() + alien.getSpeedX() * this.timeInterval * activeUpgrades.getSpeedMultiplier(), alien.getPosY() + alien.getSpeedY() * this.timeInterval * activeUpgrades.getSpeedMultiplier()));
       alien.setPhysic(new AlienNormalPhysic());
-      alien.updatePosition(remainingDt, boundary);
+      alien.updatePosition(remainingDt, boundary, activeUpgrades);
     }
   }
 
