6ac86e9093d194f8c21849a7ea48f3d2c7aafaac
marco <marcoaresu82@gmail.com>
2026-02-09 17:27:16 +0100

    Refactor AddOnPositionGenerator to accept platform and add-on dimensions; update createAddOn method to set positions for coins, enemies, and gadgets based on new parameters.

--- src/main/java/it/unibo/model/worldConstructor/impl/AddOnPositionGeneratorImpl.java
+++ src/main/java/it/unibo/model/worldConstructor/impl/AddOnPositionGeneratorImpl.java
@@ -13,19 +13,21 @@ public class AddOnPositionGeneratorImpl {
      * Generates a position for an add-on.
      * 
      * @param posPlatform the position of the platform
+     * @param platformHeight the height of the platform
+     * @param addOnHeight the height of the add-on
      * @return the calculated position for the add-on
      */
-    public Vector2d generatePosition(final Vector2d posPlatform) {
-        Vector2d pos = new Vector2dImpl(generatePosX(posPlatform), generatePosY(posPlatform));
+    public Vector2d generatePosition(final double posX, final double posY, final double platformHeight, final double addOnHeight) {
+        Vector2d pos = new Vector2dImpl(generatePosX(posX), generatePosY(posY, platformHeight, addOnHeight));
         return pos;
     }
 
-    private double generatePosX(final Vector2d posPlatform) {
-        return posPlatform.getX(); 
+    private double generatePosX(final double posX) {
+        return posX; 
     }
 
-    private double generatePosY(final Vector2d posPlatform) {
-        return posPlatform.getY(); //meta altezza piattaforma + meta altezza add-on
+    private double generatePosY(final double posY, final double platformHeight, final double addOnHeight) {
+        return posY + platformHeight / 2 + addOnHeight / 2;
     }
     
 }
