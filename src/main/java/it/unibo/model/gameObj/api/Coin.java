7b068daa38500a61408ca458c7d253474741d2b6
AndreaFronzi <andrea.fronzi@studio.unibo.it>
2026-02-09 13:27:30 +0100

    Rename `collect` method to `collectCoin` in `Coin` interface; enhance Javadoc to clarify functionality

--- src/main/java/it/unibo/model/gameObj/api/Coin.java
+++ src/main/java/it/unibo/model/gameObj/api/Coin.java
@@ -1,10 +1,13 @@
 package it.unibo.model.gameObj.api;
 
+/**
+ * Represents a Coin entity in a two-dimensional game environment which can be collected by the {@link Alien}.
+ */
 public interface Coin extends StaticEntity {
 
   /**
    * Collects the coin within the game environment.
    * This action involves removing the coin from the game world and updating the number of collected coins
    */
-  void collect();
+  void collectCoin();
 }
