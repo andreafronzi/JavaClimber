2d728341aa1f5bfeff5d6060647f8495e95ddeea
marco <marcoaresu82@gmail.com>
2026-02-09 12:50:21 +0100

    added documentation to the menu package

--- src/main/java/it/unibo/model/LaunchedGame/api/BaseLaunchedState.java
+++ src/main/java/it/unibo/model/LaunchedGame/api/BaseLaunchedState.java
@@ -1,17 +1,31 @@
 package it.unibo.model.LaunchedGame.api;
 
+/**
+ * Abstract base class for launched game states.
+ */
 public abstract class BaseLaunchedState implements StateOfLaunchedGame {
 
-    private LaunchedGame launchedGame;
+    protected LaunchedGame launchedGame;
 
+    /**
+     * Constructs a new BaseLaunchedState.
+     * 
+     * @param launchedGame the game context
+     */
     public BaseLaunchedState(final LaunchedGame launchedGame) {
         this.launchedGame = launchedGame;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void onEnter() {
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void execute() {
     }
