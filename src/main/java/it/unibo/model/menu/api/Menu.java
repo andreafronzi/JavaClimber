a98416bed21d9591cdc01adb3b9a35bedea6a7ad
marco <marcoaresu82@gmail.com>
2026-02-09 12:52:22 +0100

    added documentation to the menu package the last. The previous commit is wrong. The documentation was for launched game.

--- src/main/java/it/unibo/model/menu/api/Menu.java
+++ src/main/java/it/unibo/model/menu/api/Menu.java
@@ -1,8 +1,22 @@
 package it.unibo.model.menu.api;
 
+/**
+ * Interface representing the menu context in the State Pattern.
+ * It manages the current state of the menu and allows transitions between different states.
+ */
 public interface Menu {
 
-    public void setState();
+    /**
+     * Updates the current state of the menu.
+     * 
+     * @param state the new state to be set
+     */
+    public void setState(StateOfMenu state);
     
+    /**
+     * Retrieves the current state of the menu.
+     * 
+     * @return the currently active StateOfMenu
+     */
     public StateOfMenu getState();
 }
--- src/main/java/it/unibo/model/menu/impl/ShoppingState.java
+++ src/main/java/it/unibo/model/menu/impl/ShoppingState.java
@@ -2,12 +2,24 @@ package it.unibo.model.menu.impl;
 
 import it.unibo.model.menu.api.*;
 
+/**
+ * Represents the shopping state of the menu.
+ * Handles the logic for purchasing items or upgrades.
+ */
 public class ShoppingState extends BaseMenuState {
 
+    /**
+     * Constructs a new ShoppingState.
+     * 
+     * @param menu the Menu context
+     */
     public ShoppingState(final Menu menu) {
         super(menu);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void execute() {
         // Implementation of the execute method for ShoppingState
