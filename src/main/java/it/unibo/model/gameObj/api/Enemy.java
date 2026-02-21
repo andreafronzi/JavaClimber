1f78a8cc33191e61aa2f418c4dc0c1c26b87f0b1
AndreaFronzi <andrea.fronzi@studio.unibo.it>
2026-02-21 19:00:26 +0100

    Removing idDead method form Enemy.java and adding GameWorld param to die method

--- src/main/java/it/unibo/model/gameObj/api/Enemy.java
+++ src/main/java/it/unibo/model/gameObj/api/Enemy.java
@@ -1,5 +1,7 @@
 package it.unibo.model.gameObj.api;
 
+import it.unibo.model.world.api.GameWorld;
+
 /**
  * Represents an enemy entity in a two-dimensional game environment.
  * An enemy is a game object that has the ability to be "killed" during gameplay.
@@ -9,12 +11,8 @@ public interface Enemy extends StaticEntity {
   /**
    * Triggers the death of the enemy.
    * This method handles the removal of the enemy from the game world
+   * 
+   * @param gameWorld the {@link GameWorld} from which the enemy should be removed upon death
    */
-  void die();
-
-  /**
-   *
-   * @return whether the enemy is dead or not
-   */
-  boolean idDead();
+  void die(GameWorld gameWorld);
 }
