7fbaf0fe544f89ee5b253ab75229c1402721c81e
AndreaFronzi <andrea.fronzi@studio.unibo.it>
2026-02-22 19:47:04 +0100

    Add MoveAlienRight command to set alien speed to the right

---
+++ src/main/java/it/unibo/model/command/impl/MoveAlienRight.java
@@ -0,0 +1,16 @@
+package it.unibo.model.command.impl;
+
+import it.unibo.model.LaunchedGame.api.LaunchedGame;
+import it.unibo.model.command.api.RunningCommand;
+import it.unibo.model.gameObj.api.Alien;
+import it.unibo.model.physics.impl.Vector2dImpl;
+
+public class MoveAlienRight implements RunningCommand {
+
+  private static final double SPEED_X = 10;
+
+  @Override
+  public void execute(final Alien alien, final LaunchedGame launchedGame) {
+    alien.setSpeed(new Vector2dImpl(SPEED_X, alien.getSpeedY()));
+  }
+}
