41b8cf211eb7b085e68de84f27f5d866bdb90b78
marco <marcoaresu82@gmail.com>
2026-02-22 18:52:30 +0100

    Implement CommandState interface and add EnterPausa command for game state management

--- src/main/java/it/unibo/model/LaunchedGame/impl/RunningState.java
+++ src/main/java/it/unibo/model/LaunchedGame/impl/RunningState.java
@@ -1,6 +1,11 @@
 package it.unibo.model.LaunchedGame.impl;
 
+
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
 import it.unibo.model.LaunchedGame.api.*;
+import it.unibo.model.command.api.RunningCommand;
 import it.unibo.model.physics.collision.api.CollisionManager;
 import it.unibo.model.world.impl.World;
 
@@ -9,10 +14,11 @@ import it.unibo.model.world.impl.World;
  * Represents the state where the game is actively being played.
  * Handles the core gameplay logic.
  */
-public class RunningState extends BaseLaunchedState {
+public class RunningState extends BaseLaunchedState implements CommandState<RunningCommand> {
 
     private final World world;
     private final CollisionManager collisionManager;
+    private final Queue<RunningCommand> commands;
 
     /**
      * Constructs a new RunningState.
@@ -23,6 +29,7 @@ public class RunningState extends BaseLaunchedState {
         super(launchedGame);
         this.world = world;
         this.collisionManager = collisionManager;
+        this.commands = new ArrayBlockingQueue<>(100);
     }
 
     /**
@@ -31,10 +38,19 @@ public class RunningState extends BaseLaunchedState {
      */
     @Override
     public void execute(final double dt) {
-        world.getRealWorld().getAlien().updatePosition(0, null);
+        world.getRealWorld().getAlien().updatePosition(dt, null);
         collisionManager.detectCollisions(world.getRealWorld());
         //controlliamo se il personaggio è morto se è morto, passiamo allo stato di endstate 
         launchedGame.setState(new EndState(launchedGame));
     }
-    
+
+    @Override
+    public void addCommand(final RunningCommand command) {
+        commands.add(command);
+    }
+ 
+    private RunningCommand pollCommand() {
+        return commands.poll();
+    }
+
 }
