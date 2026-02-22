648c7f16650c10d5b369f2a57a72c8a4a665f85a
AndreaFronzi <andrea.fronzi@studio.unibo.it>
2026-02-22 18:33:33 +0100

    Add MoveAlienLeft command implementation for alien movement

---
+++ src/main/java/it/unibo/model/command/impl/MoveAlienLeft.java
@@ -0,0 +1,12 @@
+package it.unibo.model.command.impl;
+
+import it.unibo.model.command.api.RunningCommand;
+import it.unibo.model.gameObj.api.Alien;
+
+public class MoveAlienLeft implements RunningCommand {
+
+  @Override
+  public void execute(final Alien alien) {
+
+  }
+}
