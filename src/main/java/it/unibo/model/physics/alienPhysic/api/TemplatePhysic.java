6efbed33b5d3357e95f18bdd4e7015921db77508
AndreaFronzi <andrea.fronzi@studio.unibo.it>
2026-02-22 19:29:37 +0100

    Refactor verifyBoundaryTouch method in TemplatePhysic to handle both horizontal and vertical boundaries touch behavior

--- src/main/java/it/unibo/model/physics/alienPhysic/api/TemplatePhysic.java
+++ src/main/java/it/unibo/model/physics/alienPhysic/api/TemplatePhysic.java
@@ -1,10 +1,12 @@
 package it.unibo.model.physics.alienPhysic.api;
 
 import it.unibo.model.LaunchedGame.api.LaunchedGame;
+import it.unibo.model.LaunchedGame.impl.EndState;
 import it.unibo.model.gameObj.api.Alien;
 import it.unibo.model.physics.impl.Vector2dImpl;
 import it.unibo.model.shop.api.ActiveUpgrades;
 import it.unibo.model.world.api.BoundWorld;
+import it.unibo.model.world.impl.BoundY;
 import it.unibo.model.world.impl.Boundary;
 
 /**
@@ -15,7 +17,7 @@ public abstract class TemplatePhysic implements AlienPhysic {
   @Override
   public void update(final Alien alien, final double dt, final BoundWorld boundWorld, final ActiveUpgrades activeUpgrades, final LaunchedGame launchedGame) {
     moveAlien(alien, dt, boundWorld.getBoundX(), activeUpgrades);
-    verifyBoundaryTouch(alien, boundWorld.getBoundX());
+    verifyBoundaryTouch(alien, boundWorld, launchedGame);
   }
 
   /**
@@ -28,17 +30,42 @@ public abstract class TemplatePhysic implements AlienPhysic {
   protected abstract void moveAlien(final Alien alien, final double dt, final Boundary boundary, final ActiveUpgrades activeUpgrades);
 
   /**
-   * Verify if the alien go beyond the boundary. Apply pacman effect if it happens.
+   * Verify if the alien go beyond the boundary.
    *
    * @param alien the alien which can go beyond the boundary
-   * @param boundary the boundaries, which can be crossed by the alien
+   * @param boundWorld the boundaries, which can be crossed by the alien
+   * @param launchedGame the launched game, which can be ended if the alien go beyond the vertical boundary
    */
-  private void verifyBoundaryTouch(final Alien alien, final Boundary boundary) {
+  private void verifyBoundaryTouch(final Alien alien, final BoundWorld boundWorld, final LaunchedGame launchedGame) {
+    this.verifyHorizontalBoundaryTouch(alien, boundWorld.getBoundX());
+    this.verifyVerticalBoundaryTouch(alien, boundWorld.getBoundY(), launchedGame);
+  }
+
+  /**
+   * Verify if the alien go beyond the boundary. Apply pacman effect if it happens.
+   * 
+   * @param alien the {@link Alien} which can go beyond the horizontal boundary
+   * @param boundary the {@link Boundary} horizontal boundary, which can be crossed by the alien
+   */
+  private void verifyHorizontalBoundaryTouch(final Alien alien, final Boundary boundary) {
     if(alien.getPosX() + alien.getWidth() < boundary.x0()) {
       alien.setPosition(new Vector2dImpl(boundary.x1() - alien.getWidth(), alien.getPosY()));
     }
     if(alien.getPosX() > boundary.x1()) {
       alien.setPosition(new Vector2dImpl(boundary.x0(), alien.getPosY()));
+    }
+  }
+
+  /**
+   * Verify if the alien go beyond the vertical boundary. If it happens, the game is ended.
+   *
+   * @param alien the {@link Alien} which can go beyond the vertical boundary
+   * @param verticalBounds the {@link BoundY} vertical boundaries, which can be crossed by the alien
+   * @param launchedGame the {@link LaunchedGame}, which can be ended if the alien go beyond the vertical boundary
+   */
+  private void verifyVerticalBoundaryTouch(final Alien alien, final BoundY verticalBounds, final LaunchedGame launchedGame) {
+    if(alien.getPosY() + alien.getHeight() > verticalBounds.maxY()) {
+      launchedGame.setState(new EndState(launchedGame));
     }
   }
 }
