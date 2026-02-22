06c92b451510240409add5c5b66de41b1b505c17
marco <marcoaresu82@gmail.com>
2026-02-22 17:28:26 +0100

    Refactor RunningState to include World and CollisionManager dependencies in constructor and update execute method to accept dt parameter

--- src/main/java/it/unibo/model/LaunchedGame/impl/RunningState.java
+++ src/main/java/it/unibo/model/LaunchedGame/impl/RunningState.java
@@ -1,6 +1,8 @@
 package it.unibo.model.LaunchedGame.impl;
 
 import it.unibo.model.LaunchedGame.api.*;
+import it.unibo.model.physics.Collision.api.CollisionManager;
+import it.unibo.model.world.impl.World;
 
 /**
  * Represents the state where the game is actively being played.
@@ -8,13 +10,18 @@ import it.unibo.model.LaunchedGame.api.*;
  */
 public class RunningState extends BaseLaunchedState {
 
+    private final World world;
+    private final CollisionManager collisionManager;
+
     /**
      * Constructs a new RunningState.
      * 
      * @param launchedGame the game context
      */
-    public RunningState(final LaunchedGameImpl launchedGame) {
+    public RunningState(final LaunchedGameImpl launchedGame, final World world, final CollisionManager collisionManager) {
         super(launchedGame);
+        this.world = world;
+        this.collisionManager = collisionManager;
     }
 
     /**
@@ -22,8 +29,9 @@ public class RunningState extends BaseLaunchedState {
      * Executes the gameplay logic.
      */
     @Override
-    public void execute() {
-        //logica di gioco in esecuzione
+    public void execute(final double dt) {
+        world.getRealWorld().getAlien().updatePosition(0, null);
+        collisionManager.detectCollisions(world.getRealWorld());
     }
     
 }
