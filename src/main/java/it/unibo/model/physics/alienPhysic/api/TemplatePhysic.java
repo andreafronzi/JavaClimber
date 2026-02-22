784d5360bfb5c6d4ed97e1e37ac9df7aa39f4776
AndreaFronzi <andrea.fronzi@studio.unibo.it>
2026-02-22 19:19:32 +0100

    Refactor update method in TemplatePhysic class to use BoundWorld instead of Boundary

--- src/main/java/it/unibo/model/physics/alienPhysic/api/TemplatePhysic.java
+++ src/main/java/it/unibo/model/physics/alienPhysic/api/TemplatePhysic.java
@@ -4,6 +4,7 @@ import it.unibo.model.LaunchedGame.api.LaunchedGame;
 import it.unibo.model.gameObj.api.Alien;
 import it.unibo.model.physics.impl.Vector2dImpl;
 import it.unibo.model.shop.api.ActiveUpgrades;
+import it.unibo.model.world.api.BoundWorld;
 import it.unibo.model.world.impl.Boundary;
 
 /**
@@ -12,9 +13,9 @@ import it.unibo.model.world.impl.Boundary;
 public abstract class TemplatePhysic implements AlienPhysic {
 
   @Override
-  public void update(final Alien alien, final double dt, final Boundary boundary, final ActiveUpgrades activeUpgrades, final LaunchedGame launchedGame) {
-    moveAlien(alien, dt, boundary, activeUpgrades);
-    verifyBoundaryTouch(alien, boundary);
+  public void update(final Alien alien, final double dt, final BoundWorld boundWorld, final ActiveUpgrades activeUpgrades, final LaunchedGame launchedGame) {
+    moveAlien(alien, dt, boundWorld.getBoundX(), activeUpgrades);
+    verifyBoundaryTouch(alien, boundWorld.getBoundX());
   }
 
   /**
