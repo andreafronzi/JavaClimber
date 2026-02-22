b448a4b3cc961c9242298990eea5e22f75c10d9b
Pietro Siboni <pietro.siboni3@studio.unibo.it>
2026-02-22 11:13:05 +0100

    Remove unused imports in ScoreManagerTest

--- src/test/java/JavaClimber/score/ScoreManagerTest.java
+++ src/test/java/JavaClimber/score/ScoreManagerTest.java
@@ -8,8 +8,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Map;
 import java.util.Set;
