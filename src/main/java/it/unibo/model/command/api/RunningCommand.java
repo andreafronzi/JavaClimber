c6dacf58f4b354fd94d1048eddb403f2164673b1
AndreaFronzi <andrea.fronzi@studio.unibo.it>
2026-02-22 18:33:24 +0100

    Add RunningCommand interface to define executable commands for Alien objects

---
+++ src/main/java/it/unibo/model/command/api/RunningCommand.java
@@ -0,0 +1,11 @@
+package it.unibo.model.command.api;
+
+import it.unibo.model.gameObj.api.Alien;
+
+/**
+ * Represents a command that can be executed.
+ */
+public interface RunningCommand {
+
+  void execute(Alien alien);
+}
