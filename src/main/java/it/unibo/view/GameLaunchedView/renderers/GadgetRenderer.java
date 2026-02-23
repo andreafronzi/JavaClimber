be27e1efc41ef449174a77850a5234e451607298
AndreaFronzi <andrea.fronzi@studio.unibo.it>
2026-02-23 18:54:00 +0100

    Add GadgetRenderer class

---
+++ src/main/java/it/unibo/view/GameLaunchedView/renderers/GadgetRenderer.java
@@ -0,0 +1,33 @@
+package it.unibo.view.GameLaunchedView.renderers;
+
+import it.unibo.model.gameObj.api.Gadget;
+import it.unibo.view.SpriteEnum;
+import it.unibo.view.SpriteManager;
+
+import java.awt.*;
+import java.util.List;
+import java.util.Objects;
+
+public class GadgetRenderer implements EntityRenderer<Gadget> {
+
+  private final SpriteManager spriteManager;
+
+  public GadgetRenderer(final SpriteManager spriteManager) {
+    this.spriteManager = spriteManager;
+  }
+
+  @Override
+  public void render(final List<Gadget> gadgets, final Graphics2D g) {
+    final Image sprite = spriteManager.get(SpriteEnum.GADGET);
+    if (!Objects.isNull(sprite)) {
+      for (Gadget gadget : gadgets) {
+        g.drawImage(sprite,
+            (int) gadget.getPosX(),
+            (int) gadget.getPosY(),
+            (int) gadget.getWidth(),
+            (int) gadget.getHeight(),
+            null);
+      }
+    }
+  }
+}
