56ea2878b88194a63bddfdf5d0dc5f3dc6113b10
AndreaFronzi <andrea.fronzi@studio.unibo.it>
2026-02-23 17:52:19 +0100

    Enhance JavaDoc

--- src/main/java/it/unibo/view/GameLaunchedView/renderers/EntityRenderer.java
+++ src/main/java/it/unibo/view/GameLaunchedView/renderers/EntityRenderer.java
@@ -5,7 +5,18 @@ import it.unibo.model.gameObj.api.GameObject;
 import java.awt.*;
 import java.util.List;
 
+/**
+ * Interface for rendering game entities.
+ *
+ * @param <T> the type of game entity to render, which must extend {@link GameObject}
+ */
 public interface EntityRenderer<T extends GameObject> {
 
-    void render(List<T> entity, Graphics2D g);
+    /**
+     * Renders a list of entities of type T using the provided Graphics2D object.
+     * 
+     * @param entitys the list of entities to render
+     * @param g the Graphics2D object used for rendering
+     */
+    void render(List<T> entities, Graphics2D g);
 }
