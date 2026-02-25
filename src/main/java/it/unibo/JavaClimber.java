a53e7e48de2393acc3ce4cb700ab22f3a40aa85c
marco <marcoaresu82@gmail.com>
2026-02-25 16:23:44 +0100

    implemented the main

--- src/main/java/it/unibo/JavaClimber.java
+++ src/main/java/it/unibo/JavaClimber.java
@@ -1,11 +1,17 @@
 package it.unibo;
 
+import it.unibo.controller.api.MainController;
+import it.unibo.controller.impl.MainControllerImpl;
+import it.unibo.view.MainView;
+import it.unibo.view.MainViewImpl;
 
 public class JavaClimber {
 
-    public static void main(String[] args) {
-        
-
+    public static void main(final String... args) {
+        final MainController mainController = new MainControllerImpl();
+        final MainView mainView = new MainViewImpl();
+        mainController.setView(mainView);
+        mainController.openMenuView();
     }
-
+    
 }
