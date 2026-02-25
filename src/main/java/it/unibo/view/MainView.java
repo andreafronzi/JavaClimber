6c4c3bdf1535170f93f7004844e886173c1fe1c5
marco <marcoaresu82@gmail.com>
2026-02-25 10:41:08 +0100

    Implement MainView interface and its MainViewImpl class

---
+++ src/main/java/it/unibo/view/MainView.java
@@ -0,0 +1,16 @@
+package it.unibo.view;
+
+public interface MainView {
+    
+    public void setMenuView();
+
+    public void setGameLaunchedView();
+
+    public void setEndView();
+
+    public void setPauseView();
+
+    public void setInventoryView();
+
+    public void setShopView();
+}
