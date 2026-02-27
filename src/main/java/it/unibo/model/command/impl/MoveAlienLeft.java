dd53665780fc086b89dcd40f593bce29315eff50
AndreaFronzi <andrea.fronzi@studio.unibo.it>
2026-02-27 01:30:53 +0100

    Enhance JavaDoc

--- src/main/java/it/unibo/model/command/impl/MoveAlienLeft.java
+++ src/main/java/it/unibo/model/command/impl/MoveAlienLeft.java
@@ -5,10 +5,21 @@ import it.unibo.model.command.api.RunningCommand;
 import it.unibo.model.gameObj.api.Alien;
 import it.unibo.model.physics.impl.Vector2dImpl;
 
+/**
+ * <p>Rapresent an implementation of {@link RunningCommand} that move {@link Alien} left.</p>
+ */
 public class MoveAlienLeft implements RunningCommand {
 
+  /**
+   * A constant representing the horizontal speed set to the {@link Alien}.
+   */
   private static final double SPEED_X = -10;
 
+  /**
+   * <p>Move the {@link Alien} left.</p>
+   *
+   * @param alien the {@link Alien} to move.
+   */
   @Override
   public void execute(final Alien alien, final LaunchedGame launchedGame) {
     alien.setSpeed(new Vector2dImpl(SPEED_X, alien.getSpeedY()));
