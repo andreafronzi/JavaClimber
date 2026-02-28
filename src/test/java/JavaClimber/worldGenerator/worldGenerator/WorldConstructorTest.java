aa2ce27f1df778f3d9ed708b68f31a5ecc5c93a4
marco <marcoaresu82@gmail.com>
2026-02-28 15:01:42 +0100

    refactor the package of the world generator class test: addOnSpawn ├── AddOnPositiongeneratorTest.java ├── FactoryAddOnTest.java ├── platformSpawn/ │   ├── DirectorTest.java │   ├── PairTest.java │   └── PlatformPositionGeneratorTest.java ├── PlatformPoolCreatorTest.java ├── PlatformPoolTest.java └── worldGenerator/     └── WorldConstructorTest.java

---
+++ src/test/java/JavaClimber/worldGenerator/worldGenerator/WorldConstructorTest.java
@@ -0,0 +1,34 @@
+package JavaClimber.worldGenerator.worldGenerator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.unibo.model.world.api.BoundWorld;
+import it.unibo.model.world.impl.BoundWorldImpl;
+import it.unibo.model.world.impl.BoundY;
+import it.unibo.model.world.impl.Boundary;
+import it.unibo.model.world.impl.UpperWorld;
+import it.unibo.model.world.impl.World;
+import it.unibo.model.worldConstructor.data.Difficult;
+import it.unibo.model.worldConstructor.gameObjectSpawn.impl.SpawnPoolEasy;
+import it.unibo.model.worldConstructor.worldGenerator.impl.WorldConstructorImpl;
+
+public class WorldConstructorTest {
+
+    private WorldConstructorImpl worldConstructor;
+    private World world;
+    private Boundary boundX;
+    private BoundY boundY;
+    private UpperWorld upperWorld;
+    private Difficult difficult;
+
+    @BeforeEach
+    public void setUp() {
+    }
+
+    @Test
+    public void fillWorldTest() {
+    }
+}
