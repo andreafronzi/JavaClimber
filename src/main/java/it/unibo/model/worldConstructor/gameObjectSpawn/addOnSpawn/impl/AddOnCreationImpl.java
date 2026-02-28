7697fc1a2f25664cc4a3041d54e52c4c55cbee6e
marco <marcoaresu82@gmail.com>
2026-02-28 14:51:48 +0100

    Implement AddOnCreator interface and its AddOnCreatorImpl class for add-on creation logic

--- src/main/java/it/unibo/model/worldConstructor/gameObjectSpawn/addOnSpawn/impl/AddOnCreationImpl.java
+++
@@ -1,5 +0,0 @@
-package it.unibo.model.worldConstructor.gameObjectSpawn.addOnSpawn.impl;
-
-public class AddOnCreationImpl {
-    
-}
---
+++ src/main/java/it/unibo/model/worldConstructor/gameObjectSpawn/addOnSpawn/impl/AddOnCreatorImpl.java
@@ -0,0 +1,33 @@
+package it.unibo.model.worldConstructor.gameObjectSpawn.addOnSpawn.impl;
+
+import java.util.List;
+import java.util.Random;
+import java.util.function.BiConsumer;
+
+import it.unibo.model.physics.api.Vector2d;
+import it.unibo.model.physics.impl.Vector2dImpl;
+import it.unibo.model.worldConstructor.gameObjectSpawn.addOnSpawn.api.AddOnCreator;
+import it.unibo.model.worldConstructor.gameObjectSpawn.addOnSpawn.api.AddOnPool;
+import it.unibo.model.worldConstructor.gameObjectSpawn.platformSpawn.api.Pair;
+
+public class AddOnCreatorImpl implements AddOnCreator {
+
+    private List<Pair<Double, BiConsumer<Double, Vector2dImpl>>> addOnPool;
+    private final Random random;
+
+    public AddOnCreatorImpl(final AddOnPool addOnPool) {
+        this.addOnPool = addOnPool.getAddOnPool();
+        this.random = new Random();
+    }
+
+    @Override
+    public void createAddOn(final Double chance, final Vector2d position) {
+        random.nextDouble();
+        for (var addOn : addOnPool) {
+            if (chance <= addOn.getX()) {
+                addOn.getY().accept(random.nextDouble(1.0), new Vector2dImpl(position.getX(), position.getY()));
+            }
+        }
+    }
+    
+}
