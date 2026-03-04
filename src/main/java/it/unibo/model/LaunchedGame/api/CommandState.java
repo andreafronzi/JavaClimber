5d5e664913f0c95a3e11d5fa37de7e30c948b10b
marco <marcoaresu82@gmail.com>
2026-03-04 18:48:39 +0100

    Change the pollCommand method to restore an Optional<X> instead of X

--- src/main/java/it/unibo/model/LaunchedGame/api/CommandState.java
+++ src/main/java/it/unibo/model/LaunchedGame/api/CommandState.java
@@ -1,9 +1,11 @@
 package it.unibo.model.LaunchedGame.api;
 
+import java.util.Optional;
+
 public interface CommandState<X> {
 
     void addCommand(X command);
 
-    X pollCommand();
+    Optional<X> pollCommand();
 
 }
