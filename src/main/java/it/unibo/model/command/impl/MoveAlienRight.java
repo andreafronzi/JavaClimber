f9715343b7f6310f8a60c32a6db3d31bdec535c5
AndreaFronzi <andrea.fronzi@studio.unibo.it>
2026-03-04 23:38:05 +0100

    Simplify MoveAlienRight command by removing speed constant and directly invoking moveRight method

--- src/main/java/it/unibo/model/command/impl/MoveAlienRight.java
+++ src/main/java/it/unibo/model/command/impl/MoveAlienRight.java
@@ -3,7 +3,6 @@ package it.unibo.model.command.impl;
 import it.unibo.model.LaunchedGame.api.LaunchedGame;
 import it.unibo.model.command.api.RunningCommand;
 import it.unibo.model.gameObj.api.Alien;
-import it.unibo.model.physics.impl.Vector2dImpl;
 
 /**
  * <p>Rapresent an implementation of {@link RunningCommand} that move {@link Alien} right.</p>
@@ -11,15 +10,10 @@ import it.unibo.model.physics.impl.Vector2dImpl;
 public class MoveAlienRight implements RunningCommand {
 
   /**
-   * A constant representing the horizontal speed set to the {@link Alien}.
-   */
-  private static final double SPEED_X = 400.0;
-
-  /**
    * <p>Move the {@link Alien} right.</p>
    */
   @Override
   public void execute(final Alien alien, final LaunchedGame launchedGame) {
-    alien.setSpeed(new Vector2dImpl(SPEED_X, alien.getSpeedY()));
+    alien.moveRight();
   }
 }
