5dc35dd9b7b29773b2e2dc1280ef0a6572321784
AndreaFronzi <andrea.fronzi@studio.unibo.it>
2026-03-04 23:38:16 +0100

    Refactor StopAlienMovement command to directly stop alien movement, removing unnecessary speed handling

--- src/main/java/it/unibo/model/command/impl/StopAlienMovement.java
+++ src/main/java/it/unibo/model/command/impl/StopAlienMovement.java
@@ -3,23 +3,17 @@ package it.unibo.model.command.impl;
 import it.unibo.model.LaunchedGame.api.LaunchedGame;
 import it.unibo.model.command.api.RunningCommand;
 import it.unibo.model.gameObj.api.Alien;
-import it.unibo.model.physics.impl.Vector2dImpl;
 
 /**
- * Rapresent an implementation of {@link RunningCommand} that stop alien movement.
+ * <p>Rapresent an implementation of {@link RunningCommand} that stop alien movement.</p>
  */
 public class StopAlienMovement implements RunningCommand {
-
-  /**
-   * A constant representing a null speed value.
-   */
-  private static final double NULL_SPEED = 0;
 
   /**
    * Stop alien movement.
    */
   @Override
   public void execute(final Alien alien, final LaunchedGame launchedGame) {
-    alien.setSpeed(new Vector2dImpl(NULL_SPEED, alien.getSpeedY()));
+    alien.stopMoving();
   }
 }
