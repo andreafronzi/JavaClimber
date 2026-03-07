d8e33386f734a4166b03889e5c30229da2e47f13
marco <marcoaresu82@gmail.com>
2026-03-07 20:43:15 +0100

    Added PlatformCreator and PlatformPool interfaces, and implemented PlatformCreatorImpl and PlatformPoolEasy for managing platforms in the game

---
+++ src/main/java/it/unibo/model/worldConstructor/gameObjectSpawn/platformSpawn/api/PlatformPool.java
@@ -0,0 +1,25 @@
+package it.unibo.model.worldConstructor.gameObjectSpawn.platformSpawn.api;
+
+import java.util.List;
+import java.util.function.BiConsumer;
+
+import it.unibo.model.physics.api.Vector2d;
+import it.unibo.model.worldConstructor.gameObjectSpawn.api.SpawnPoolCreator;
+
+public interface PlatformPool {
+    
+    /**
+     * Gets the pool of platforms, which is a list of pairs containing a probability and a platform creation function.
+     * 
+     * @return the list of pairs representing the platform pool
+     */
+    public List<Pair<Double, BiConsumer<Double, Vector2d>>> getPlatformPool();
+
+    void setSpawnPoolCreator(SpawnPoolCreator spawnPoolCreator);
+
+    double getChancePlatform();
+
+    double getWidth();
+
+    double getHeight();
+}
---
+++ src/main/java/it/unibo/model/worldConstructor/gameObjectSpawn/platformSpawn/impl/PlatformCreatorImpl.java
@@ -0,0 +1,41 @@
+package it.unibo.model.worldConstructor.gameObjectSpawn.platformSpawn.impl;
+
+import java.util.List;
+import java.util.Random;
+import java.util.function.BiConsumer;
+
+import it.unibo.model.physics.api.Vector2d;
+import it.unibo.model.physics.impl.Vector2dImpl;
+import it.unibo.model.worldConstructor.gameObjectSpawn.platformSpawn.api.Pair;
+import it.unibo.model.worldConstructor.gameObjectSpawn.platformSpawn.api.PlatformCreator;
+import it.unibo.model.worldConstructor.gameObjectSpawn.platformSpawn.api.PlatformPool;
+
+public class PlatformCreatorImpl implements PlatformCreator {
+
+    private List<Pair<Double, BiConsumer<Double, Vector2d>>> platformPool;
+    private final Random random;
+
+    public PlatformCreatorImpl(final PlatformPool platformPool) {
+        this.platformPool = platformPool.getPlatformPool();
+        this.random = new Random();
+    }
+
+
+    @Override
+    public void createPlatform(double chance, Vector2d pos) {
+        random.nextDouble();
+        for (var platform : platformPool) {
+            if (chance <= platform.getX()) {
+                platform.getY().accept(random.nextDouble(1.0), new Vector2dImpl(pos.getX(), pos.getY()));
+                break;
+            }
+        }
+    }
+
+
+    @Override
+    public void setPlatformPool(PlatformPool platformPool) {
+        this.platformPool = platformPool.getPlatformPool();
+    }
+    
+}
