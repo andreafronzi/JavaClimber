baee4108da7e1d4e5f33a7d139feccca63d3fe7f
marco <marcoaresu82@gmail.com>
2026-03-07 20:44:14 +0100

    Changed AddOnCreator interface to use Platform instead of Vector2d and updated AddOnPool and implementations

--- src/main/java/it/unibo/model/worldConstructor/gameObjectSpawn/addOnSpawn/api/AddOnCreator.java
+++ src/main/java/it/unibo/model/worldConstructor/gameObjectSpawn/addOnSpawn/api/AddOnCreator.java
@@ -1,10 +1,11 @@
 package it.unibo.model.worldConstructor.gameObjectSpawn.addOnSpawn.api;
 
+import it.unibo.model.gameObj.api.Platform;
 import it.unibo.model.physics.api.Vector2d;
 
 public interface AddOnCreator {
 
-    public void selectAddOn(Double chance, Vector2d position);
+    public void selectAddOn(Double chance, Platform platform);
 
     public void setAddOnPool(AddOnPool addOnPool);
 
