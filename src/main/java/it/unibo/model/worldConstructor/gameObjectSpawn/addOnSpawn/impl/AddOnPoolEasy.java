0855a79488c19b91ca745ea883bc13f74194d8df
marco <marcoaresu82@gmail.com>
2026-03-11 17:24:07 +0100

    Refactored AddOnPoolEasy to extend AddOnPoolBase

--- src/main/java/it/unibo/model/worldConstructor/gameObjectSpawn/addOnSpawn/impl/AddOnPoolEasy.java
+++ src/main/java/it/unibo/model/worldConstructor/gameObjectSpawn/addOnSpawn/impl/AddOnPoolEasy.java
@@ -1,49 +1,23 @@
 package it.unibo.model.worldConstructor.gameObjectSpawn.addOnSpawn.impl;
 
-import java.util.LinkedList;
-import java.util.List;
 import java.util.function.BiConsumer;
 
 import it.unibo.model.gameObj.api.Platform;
-import it.unibo.model.physics.impl.Vector2dImpl;
-import it.unibo.model.worldConstructor.gameObjectSpawn.addOnSpawn.api.AddOnPool;
+import it.unibo.model.worldConstructor.gameObjectSpawn.addOnSpawn.api.AddOnPoolBase;
 import it.unibo.model.worldConstructor.gameObjectSpawn.api.SpawnPoolCreator;
-import it.unibo.model.worldConstructor.gameObjectSpawn.platformSpawn.api.Pair;
 import it.unibo.model.worldConstructor.gameObjectSpawn.platformSpawn.impl.PairImpl;
 
-public class AddOnPoolEasy implements AddOnPool {
-
-    private final List<Pair<Double,BiConsumer<Double,Platform>>> addOnPool;
-    private SpawnPoolCreator spawnPoolCreator;
-    private final double chanceAddOn;
+public class AddOnPoolEasy extends AddOnPoolBase {
 
     public AddOnPoolEasy(final SpawnPoolCreator spawnPoolCreator, final double chanceAddOn) {
-        this.addOnPool = new LinkedList<>();
-        this.chanceAddOn = chanceAddOn;
-        this.spawnPoolCreator = spawnPoolCreator;
+        super(spawnPoolCreator, chanceAddOn);
         this.createAddOn();
-    }
-
-    @Override
-    public List<Pair<Double,BiConsumer<Double,Platform>>> getAddOnPool() {
-        return this.addOnPool;
-    }
-    
-    @Override
-    public double getChanceAddOn() {
-        return this.chanceAddOn;
     }
 
     private void createAddOn() {
         this.addOnPool.add(new PairImpl<Double,BiConsumer<Double,Platform>>(0.2, this.spawnPoolCreator::createGadget));
         this.addOnPool.add(new PairImpl<Double,BiConsumer<Double,Platform>>(0.4, this.spawnPoolCreator::createMonster));
         this.addOnPool.add(new PairImpl<Double,BiConsumer<Double,Platform>>(1.0, this.spawnPoolCreator::createMoney));
-    }
-
-    @Override
-    public void setSpawnPoolCreator(final SpawnPoolCreator spawnPoolCreator) {
-        this.spawnPoolCreator = spawnPoolCreator;
-        this.createAddOn();
     }
     
 }
