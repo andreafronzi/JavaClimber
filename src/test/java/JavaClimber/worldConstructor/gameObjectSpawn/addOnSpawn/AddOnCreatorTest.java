db981565e6dfd11c78e1c684df9976b5f9f86d0b
marco <marcoaresu82@gmail.com>
2026-03-11 17:32:39 +0100

    fix the signs of the assert to observe the world architecture

--- src/test/java/JavaClimber/worldConstructor/gameObjectSpawn/addOnSpawn/AddOnCreatorTest.java
+++ src/test/java/JavaClimber/worldConstructor/gameObjectSpawn/addOnSpawn/AddOnCreatorTest.java
@@ -2,12 +2,16 @@ package JavaClimber.worldConstructor.gameObjectSpawn.addOnSpawn;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Optional;
 import java.util.Random;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import it.unibo.model.gameObj.api.Platform;
+import it.unibo.model.gameObj.impl.PlatformImpl;
 import it.unibo.model.physics.impl.Vector2dImpl;
+import it.unibo.model.score.impl.ScoreManagerImpl;
 import it.unibo.model.world.api.QueueWorld;
 import it.unibo.model.world.impl.BoundWorldImpl;
 import it.unibo.model.world.impl.BoundY;
@@ -33,38 +37,39 @@ public class AddOnCreatorTest {
     private static final double POS_X = 200;
     private static final double POS_Y = 700;
 
-    private static final double CHANCE_ADDON = 0.3;
+    private static final double CHANCE_ADDON = 1.0;
     
     private Random random;
     private QueueWorld world;
     private SpawnPoolCreator spawnPoolCreator;
     private AddOnCreator addOnCreator;
+    private Platform platform;
 
     @BeforeEach
     public void setUp(){
         this.random = new Random();
         this.world = new UpperWorld(new BoundWorldImpl(new BoundY(Y_MIN, Y_MAX), new Boundary(X_MIN, X_MAX)));
         this.spawnPoolCreator = new SpawnPoolCreatorImpl(world);
-        this.spawnPoolCreator.setSpawnPool(new SpawnPoolEasy(PLATFORM_WIDTH, PLATFORM_HEIGHT));
+        this.spawnPoolCreator.setSpawnPool(new SpawnPoolEasy(PLATFORM_WIDTH, PLATFORM_HEIGHT, new ScoreManagerImpl()));
         this.addOnCreator = new AddOnCreatorImpl(new AddOnPoolEasy(this.spawnPoolCreator, CHANCE_ADDON));
+        this.platform = new PlatformImpl(new Vector2dImpl(POS_X, POS_Y), PLATFORM_WIDTH, PLATFORM_HEIGHT, Optional.empty(), Optional.empty());
     }
 
     @Test
     public void createAddOnTest(){
-        var pos = new Vector2dImpl(POS_X, POS_Y);
-        this.addOnCreator.selectAddOn(random.nextDouble(1), pos);
+        this.addOnCreator.selectAddOn(random.nextDouble(1.0), this.platform);
         if(this.world.getGadgets().size() > 0){
             var gadget = this.world.removeFirstGadget().get();
             assertEquals(POS_X + ((PLATFORM_WIDTH - gadget.getWidth()) / 2), gadget.getPosX());
-            assertEquals(POS_Y + gadget.getHeight() , gadget.getPosY());
+            assertEquals(POS_Y - gadget.getHeight() , gadget.getPosY());
         } else if(this.world.getMoneys().size() > 0){
             var money = this.world.removeFirstMoney().get();
             assertEquals(POS_X + ((PLATFORM_WIDTH - money.getWidth()) / 2), money.getPosX());
-            assertEquals(POS_Y + money.getHeight() , money.getPosY());
+            assertEquals(POS_Y - money.getHeight() , money.getPosY());
         } else {
             var monster = this.world.removeFirstMonster().get();
             assertEquals(POS_X + ((PLATFORM_WIDTH - monster.getWidth()) / 2), monster.getPosX());
-            assertEquals(POS_Y + monster.getHeight() , monster.getPosY());
+            assertEquals(POS_Y - monster.getHeight() , monster.getPosY());
         }
     }
 
