34a9088414d23cc344e0290fa9a8c019bbd6fccd
marco <marcoaresu82@gmail.com>
2026-03-11 19:01:23 +0100

    fix dimensions for alien and platform objects in GameObjDimension

--- src/main/java/it/unibo/model/worldConstructor/gameObjectSpawn/addOnSpawn/impl/GameObjDimension.java
+++ src/main/java/it/unibo/model/worldConstructor/gameObjectSpawn/addOnSpawn/impl/GameObjDimension.java
@@ -1,11 +1,8 @@
 package it.unibo.model.worldConstructor.gameObjectSpawn.addOnSpawn.impl;
 
 public class GameObjDimension {
-    public static final double ALIEN_WIDTH = 30;
-    public static final double ALIEN_HEIGHT = 30;
-
-    public static final double PLATFORM_WIDTH = 30;
-    public static final double PLATFORM_HEIGHT = 10;
+    public static final double ALIEN_WIDTH = 100;
+    public static final double ALIEN_HEIGHT = 100;
 
     public static final double COIN_WIDTH = 20;
     public static final double COIN_HEIGHT = 20;
@@ -16,11 +13,11 @@ public class GameObjDimension {
     public static final double ELYCAP_WIDTH = 30;
     public static final double ELYCAP_HEIGHT = 30;
 
-    public static final double EASY_PLATFORM_WIDTH = 30;
-    public static final double EASY_PLATFORM_HEIGHT = 10;
+    public static final double EASY_PLATFORM_WIDTH = 100;
+    public static final double EASY_PLATFORM_HEIGHT = 20;
 
-    public static final double MEDIUM_PLATFORM_WIDTH = 30;
-    public static final double MEDIUM_PLATFORM_HEIGHT = 10;
+    public static final double MEDIUM_PLATFORM_WIDTH = 100;
+    public static final double MEDIUM_PLATFORM_HEIGHT = 20;
 
     public static final double HARD_PLATFORM_WIDTH = 30;
     public static final double HARD_PLATFORM_HEIGHT = 10;
