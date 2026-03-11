0e389392924329bb27fbdaa994491c5d1027f19c
marco <marcoaresu82@gmail.com>
2026-03-11 17:30:40 +0100

    Refactor queueworldtest

--- src/test/java/JavaClimber/world/QueueWorldTest.java
+++ src/test/java/JavaClimber/world/QueueWorldTest.java
@@ -2,6 +2,7 @@ package JavaClimber.world;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import it.unibo.model.world.impl.BoundWorldImpl;
@@ -15,50 +16,81 @@ import it.unibo.model.gameObj.api.Gadget;
 import it.unibo.model.gameObj.api.Platform;
 import it.unibo.model.physics.api.Vector2d;
 import it.unibo.model.physics.impl.Vector2dImpl;
+import it.unibo.model.physics.platformPhysic.impl.HorizontalMovementBehavior;
+import it.unibo.model.physics.platformPhysic.impl.OnTouchDestroyBehavior;
 import it.unibo.model.gameObj.impl.CoinImpl;
 import it.unibo.model.gameObj.impl.EliCap;
 import it.unibo.model.gameObj.impl.EnemyImpl;
-
 
 public class QueueWorldTest {
 
     private UpperWorld upperWorld;
 
+    private static final double POS_X = 0;
+    private static final double POS_Y = 0;
+
+    private static final double Y_MIN = 0;
+    private static final double Y_MAX = 800;
+
+    private static final double X_MIN = 0;
+    private static final double X_MAX = 600;
+
+    @BeforeEach
     private void setUpUpperWorld() {
-        this.upperWorld = new UpperWorld(new BoundWorldImpl(new BoundY(0, 800), new Boundary(0, 600)));
+        this.upperWorld = new UpperWorld(new BoundWorldImpl(new BoundY(Y_MIN, Y_MAX), new Boundary(X_MIN, X_MAX)));
     }
 
-    private Platform createPlatform(final Vector2d pos) {
+    private Platform createStaticPlatform(final Vector2d pos) {
         PlatformBuilderImpl platformBuilder = new PlatformBuilderImpl();
         Platform platform = platformBuilder.at(pos).build();
         return platform;
     }
 
+    private Platform createMovingPlatform(final Vector2d pos) {
+        PlatformBuilderImpl platformBuilder = new PlatformBuilderImpl();
+        Platform platform = platformBuilder.at(pos).addMovementBehaviour(new HorizontalMovementBehavior(100)).build();
+        return platform;
+    }
+
+    private Platform createOnToucPlatform(final Vector2d pos) {
+        PlatformBuilderImpl platformBuilder = new PlatformBuilderImpl();
+        Platform platform = platformBuilder.at(pos).addJumpBehaviour(new OnTouchDestroyBehavior()).build();
+        return platform;
+    }
+
     @Test
-    public void addPlatformTest() {
-        setUpUpperWorld();
-        Platform platform = createPlatform(new Vector2dImpl(0, 0));
-        assertEquals(true, this.upperWorld.addPlatform(platform));
+    public void addStaticPlatformTest() {
+        Platform platform = createStaticPlatform(new Vector2dImpl(POS_X, POS_Y));
+        assertEquals(true, this.upperWorld.addStaticPlatform(platform));
+    }
+
+    @Test
+    public void addMovingPlatformTest() {
+        Platform platform = createMovingPlatform(new Vector2dImpl(POS_X, POS_Y));
+        assertEquals(true, this.upperWorld.addMovingPlatform(platform));
+    }
+
+    @Test
+    public void addOnTouchPlatformTest() {
+        Platform platform = createOnToucPlatform(new Vector2dImpl(POS_X, POS_Y));
+        assertEquals(true, this.upperWorld.addOnTouchPlatform(platform));
     }
 
     @Test
     public void addMonsterTest() {
-        setUpUpperWorld();
-        Enemy monster = new EnemyImpl(0, 0, new Vector2dImpl(0, 0));
+        Enemy monster = new EnemyImpl(0, 0, new Vector2dImpl(POS_X, POS_Y));
         assertEquals(true, this.upperWorld.addMonster(monster));
     }
 
     @Test
     public void addGadgetTest() {
-        setUpUpperWorld();
-        Gadget gadget = new EliCap(0, 0, new Vector2dImpl(0, 0));
+        Gadget gadget = new EliCap(0, 0, new Vector2dImpl(POS_X, POS_Y));
         assertEquals(true, this.upperWorld.addGadget(gadget));
     }
 
     @Test
     public void addMoneyTest() {
-        setUpUpperWorld();
-        Coin money = new CoinImpl(0, 0, null, null);
+        Coin money = new CoinImpl(0, 0, new Vector2dImpl(POS_X, POS_Y), null);
         assertEquals(true, this.upperWorld.addMoney(money));
     }
 
@@ -72,33 +104,43 @@ public class QueueWorldTest {
      */
 
     @Test
-    public void removeFirstPlatformTest() {
-        setUpUpperWorld();
-        Platform platform = createPlatform(new Vector2dImpl(0, 0));
-        this.upperWorld.addPlatform(platform);
-        assertEquals(platform, this.upperWorld.removeFirstPlatform().get());
+    public void removeFirstStaticPlatformTest() {
+        Platform platform = createStaticPlatform(new Vector2dImpl(POS_X, POS_Y));
+        this.upperWorld.addStaticPlatform(platform);
+        assertEquals(platform, this.upperWorld.removeFirstStaticPlatform().get());
+    }
+
+    @Test
+    public void removeFirstMovingPlatformTest() {
+        Platform platform = createMovingPlatform(new Vector2dImpl(POS_X, POS_Y));
+        this.upperWorld.addMovingPlatform(platform);
+        assertEquals(platform, this.upperWorld.removeFirstMovingPlatform().get());
+    }
+
+    @Test
+    public void removeFirstOnTouchPlatformTest() {
+        Platform platform = createOnToucPlatform(new Vector2dImpl(POS_X, POS_Y));
+        this.upperWorld.addOnTouchPlatform(platform);
+        assertEquals(platform, this.upperWorld.removeFirstOnTouchPlatform().get());
     }
 
     @Test
     public void removeFirstMonsterTest() {
-        setUpUpperWorld();
-        Enemy monster = new EnemyImpl(0, 0, new Vector2dImpl(0, 0));
+        Enemy monster = new EnemyImpl(0, 0, new Vector2dImpl(POS_X, POS_Y));
         this.upperWorld.addMonster(monster);
         assertEquals(monster, this.upperWorld.removeFirstMonster().get());
     }
 
     @Test
     public void removeFirstGadgetTest() {
-        setUpUpperWorld();
-        Gadget gadget = new EliCap(0, 0, new Vector2dImpl(0, 0));
+        Gadget gadget = new EliCap(0, 0, new Vector2dImpl(POS_X, POS_Y));
         this.upperWorld.addGadget(gadget);
         assertEquals(gadget, this.upperWorld.removeFirstGadget().get());
     }
 
     @Test
     public void removeFirstCoinTest() {
-        setUpUpperWorld();
-        Coin coin = new CoinImpl(0, 0, null, null);
+        Coin coin = new CoinImpl(0, 0, new Vector2dImpl(POS_X, POS_Y), null);
         this.upperWorld.addMoney(coin);
         assertEquals(coin, this.upperWorld.removeFirstMoney().get());
     }
@@ -114,33 +156,43 @@ public class QueueWorldTest {
      */
 
     @Test
-    public void getPlatformsTest() {
-        setUpUpperWorld();
-        Platform platform = createPlatform(new Vector2dImpl(0, 0));
-        this.upperWorld.addPlatform(platform);
-        assertEquals(1, this.upperWorld.getPlatforms().size());
+    public void getStaticPlatformsTest() {
+        Platform platform = createStaticPlatform(new Vector2dImpl(POS_X, POS_Y));
+        this.upperWorld.addStaticPlatform(platform);
+        assertEquals(1, this.upperWorld.getStaticPlatforms().size());
+    }
+
+    @Test
+    public void getMovingPlatformsTest() {
+        Platform platform = createMovingPlatform(new Vector2dImpl(POS_X, POS_Y));
+        this.upperWorld.addMovingPlatform(platform);
+        assertEquals(1, this.upperWorld.getMovingPlatforms().size());
+    }
+
+    @Test
+    public void getOnTouchPlatformsTest() {
+        Platform platform = createOnToucPlatform(new Vector2dImpl(POS_X, POS_Y));
+        this.upperWorld.addOnTouchPlatform(platform);
+        assertEquals(1, this.upperWorld.getOnTouchPlatforms().size());
     }
 
     @Test
     public void getMonstersTest() {
-        setUpUpperWorld();
-        Enemy monster = new EnemyImpl(0, 0, new Vector2dImpl(0, 0));
+        Enemy monster = new EnemyImpl(0, 0, new Vector2dImpl(POS_X, POS_Y));
         this.upperWorld.addMonster(monster);
         assertEquals(1, this.upperWorld.getMonsters().size());
     }
 
     @Test
     public void getGadgetsTest() {
-        setUpUpperWorld();
-        Gadget gadget = new EliCap(0, 0, new Vector2dImpl(0, 0));
+        Gadget gadget = new EliCap(0, 0, new Vector2dImpl(POS_X, POS_Y));
         this.upperWorld.addGadget(gadget);
         assertEquals(1, this.upperWorld.getGadgets().size());
     }
 
     @Test
     public void getCoinsTest() {
-        setUpUpperWorld();
-        Coin coin = new CoinImpl(0, 0, null, null);
+        Coin coin = new CoinImpl(0, 0, new Vector2dImpl(POS_X, POS_Y), null);
         this.upperWorld.addMoney(coin);
         assertEquals(1, this.upperWorld.getMoneys().size());
     }
