4b61ccb9781043a76035bd8c408546a81a12d6e9
marco <marcoaresu82@gmail.com>
2026-03-21 16:51:32 +0100

    created and implemented the class addOnPoolMedium

---
+++ src/main/java/it/unibo/model/worldConstructor/gameObjectSpawn/addOnSpawn/impl/AddOnPoolMedium.java
@@ -0,0 +1,24 @@
+package it.unibo.model.worldConstructor.gameObjectSpawn.addOnSpawn.impl;
+
+import java.util.function.BiConsumer;
+
+import it.unibo.model.gameObj.api.Platform;
+import it.unibo.model.worldConstructor.gameObjectSpawn.addOnSpawn.api.AddOnPoolBase;
+import it.unibo.model.worldConstructor.gameObjectSpawn.api.SpawnPoolCreator;
+import it.unibo.model.worldConstructor.gameObjectSpawn.platformSpawn.impl.PairImpl;
+
+
+public class AddOnPoolMedium extends AddOnPoolBase {
+
+     public AddOnPoolMedium(final SpawnPoolCreator spawnPoolCreator, final double chanceAddOn) {
+        super(spawnPoolCreator, chanceAddOn);
+        this.createAddOn();
+    }
+
+    private void createAddOn() {
+        this.addOnPool.add(new PairImpl<Double,BiConsumer<Double,Platform>>(0.2, this.spawnPoolCreator::createGadget));
+        this.addOnPool.add(new PairImpl<Double,BiConsumer<Double,Platform>>(0.4, this.spawnPoolCreator::createMonster));
+        this.addOnPool.add(new PairImpl<Double,BiConsumer<Double,Platform>>(1.0, this.spawnPoolCreator::createMoney));
+    }
+    
+}
