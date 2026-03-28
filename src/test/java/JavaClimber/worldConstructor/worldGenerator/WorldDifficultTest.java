6aea0b670a0c54857c47fe9b94d149591c16c7ce
marco <marcoaresu82@gmail.com>
2026-03-28 16:31:21 +0100

    add the method setup and testdifficult

--- src/test/java/JavaClimber/worldConstructor/worldGenerator/WorldDifficultTest.java
+++ src/test/java/JavaClimber/worldConstructor/worldGenerator/WorldDifficultTest.java
@@ -1,5 +1,18 @@
 package JavaClimber.worldConstructor.worldGenerator;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class WorldDifficultTest {
     
+    @BeforeEach
+    public void setUp() {
+    
+    }
+
+    @Test
+    public void testDifficult() {
+         
+    }
+
 }
