608a3d9ab500bcee293a81b41bf0befbe9adbfdf
Pietro Siboni <pietro.siboni3@studio.unibo.it>
2026-03-28 15:33:53 +0100

    Update Persistence part second the build warning

--- src/main/java/it/unibo/model/persistence/api/SaveManager.java
+++ src/main/java/it/unibo/model/persistence/api/SaveManager.java
@@ -8,13 +8,15 @@ import java.util.Optional;
 public interface SaveManager {
 
     /**
-     * Saves the current game state
+     * Saves the current game state.
+     * 
      * @param state the state to save
      */
     void save(SaveState state);
 
     /**
-     * Load the game state
+     * Load the game state.
+     * 
      * @return an Optional containing the saved file if it exists
      */
     Optional<SaveState> load();
