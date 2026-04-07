9565f5f36895d14443dcf069f5da6b1e030e7a31
marco <marcoaresu82@gmail.com>
2026-04-07 22:09:20 +0200

    added java doc

--- src/main/java/it/unibo/model/worldConstructor/gameObjectSpawn/api/SpawnPool.java
+++ src/main/java/it/unibo/model/worldConstructor/gameObjectSpawn/api/SpawnPool.java
@@ -18,37 +18,48 @@ import it.unibo.model.worldConstructor.gameObjectSpawn.platformSpawn.api.Pair;
 public interface SpawnPool {
 
     /**
-     * Retrieves the pool of available platforms.
+     * Retrieves the pool of available static platforms.
      * 
-     * @return a list of pairs containing platform factories and their spawn chances
+     * @return a list of pairs containing static platform factories and their spawn
+     *         chances
      */
-    public List<Pair<Double,Function<Vector2d,Platform>>> getStaticPlatformPool();
+    List<Pair<Double, Function<Vector2d, Platform>>> getStaticPlatformPool();
 
-    public List<Pair<Double,Function<Vector2d,Platform>>> getMovingPlatformPool();
+    /**
+     * Retrieves the pool of available moving platforms.
+     * 
+     * @return a list of pairs containing moving platform factories and their spawn
+     *         chances
+     */
+    List<Pair<Double, Function<Vector2d, Platform>>> getMovingPlatformPool();
 
-    public List<Pair<Double,Function<Vector2d,Platform>>> getOnTouchPlatformPool();
+    /**
+     * Retrieves the pool of available on-touch platforms.
+     * 
+     * @return a list of pairs containing on-touch platform factories and their
+     *         spawn chances
+     */
+    List<Pair<Double, Function<Vector2d, Platform>>> getOnTouchPlatformPool();
 
     /**
      * Retrieves the pool of available enemies (monsters).
      * 
      * @return a list of pairs containing enemy factories and their spawn chances
      */
-    public List<Pair<Double,Function<Vector2d,Enemy>>> getMonsterPool();
+    List<Pair<Double, Function<Vector2d, Enemy>>> getMonsterPool();
 
     /**
      * Retrieves the pool of available gadgets.
      * 
      * @return a list of pairs containing gadget factories and their spawn chances
      */
-    public List<Pair<Double,Function<Vector2d,Gadget>>> getGadgetPool();
+    List<Pair<Double, Function<Vector2d, Gadget>>> getGadgetPool();
 
     /**
      * Retrieves the pool of available coins (money).
      * 
      * @return a list of pairs containing coin factories and their spawn chances
      */
-    public List<Pair<Double,Function<Vector2d,Coin>>> getMoneyPool();
-
-    // public List<Pair<Double,Function<Vector2d,Trap>>> getTrapPool();
+    List<Pair<Double, Function<Vector2d, Coin>>> getMoneyPool();
 
 }
