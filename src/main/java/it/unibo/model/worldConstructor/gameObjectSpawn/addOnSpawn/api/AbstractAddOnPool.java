14e761b3a1c6f9a2f72799a7ef6986ec42701948
marco <marcoaresu82@gmail.com>
2026-04-07 22:07:54 +0200

    added java doc

--- src/main/java/it/unibo/model/worldConstructor/gameObjectSpawn/addOnSpawn/api/AbstractAddOnPool.java
+++ src/main/java/it/unibo/model/worldConstructor/gameObjectSpawn/addOnSpawn/api/AbstractAddOnPool.java
@@ -8,23 +8,51 @@ import it.unibo.model.gameObj.api.Platform;
 import it.unibo.model.worldConstructor.gameObjectSpawn.api.SpawnPoolCreator;
 import it.unibo.model.worldConstructor.gameObjectSpawn.platformSpawn.api.Pair;
 
-public abstract class AddOnPoolBase implements AddOnPool {
+/**
+ * Base implementation of {@link AddOnPool}.
+ */
+public abstract class AbstractAddOnPool implements AddOnPool {
 
+    /**
+     * List of pairs where the first element is the probability of spawning the
+     * add-on and the second element is a BiConsumer that takes the time and the
+     * platform on which to spawn the add-on.
+     */
     protected final List<Pair<Double, BiConsumer<Double, Platform>>> addOnPool;
+
+    /**
+     * The spawn pool creator used to create the spawn pool for the add-ons.
+     */
     protected final SpawnPoolCreator spawnPoolCreator;
+
+    /**
+     * The probability of spawning an add-on.
+     */
     private final double chanceAddOn;
 
-    public AddOnPoolBase(final SpawnPoolCreator spawnPoolCreator, final double chanceAddOn) {
+    /**
+     * Constructor for AddOnPoolBase.
+     *
+     * @param spawnPoolCreator the spawn pool creator used to create the spawn pool for the add-ons
+     * @param chanceAddOn      the probability of spawning an add-on
+     */
+    public AbstractAddOnPool(final SpawnPoolCreator spawnPoolCreator, final double chanceAddOn) {
         this.addOnPool = new LinkedList<>();
         this.chanceAddOn = chanceAddOn;
         this.spawnPoolCreator = spawnPoolCreator;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public List<Pair<Double, BiConsumer<Double, Platform>>> getAddOnPool() {
         return this.addOnPool;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public double getChanceAddOn() {
         return this.chanceAddOn;
