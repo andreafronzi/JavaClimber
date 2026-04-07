d204a267e6b83bcc190d8fa1f1984f2f9599b000
marco <marcoaresu82@gmail.com>
2026-04-07 22:07:48 +0200

    added java doc

--- src/main/java/it/unibo/model/worldConstructor/data/Difficult.java
+++ src/main/java/it/unibo/model/worldConstructor/data/Difficult.java
@@ -8,20 +8,11 @@ import it.unibo.model.worldConstructor.gameObjectSpawn.platformSpawn.impl.Distan
 /**
  * Record representing the configuration for a specific difficulty level.
  * 
- * @param height the height at which this difficulty becomes active
- * @param maxDistanceY maximum vertical distance between platforms
- * @param minDistanceY minimum vertical distance between platforms
- * @param maxDistanceX maximum horizontal distance for platform placement
- * @param chanceAddOn probability of an add-on appearing on a platform
- * @param coinChance probability of a coin spawning
- * @param maxMoney maximum amount of money allowed
- * @param monsterChance probability of a monster spawning
- * @param maxMonster maximum number of monsters allowed
- * @param gadgetChance probability of a gadget spawning
- * @param maxGadget maximum number of gadgets allowed
- * @param trapChance probability of a trap spawning
- * @param maxTrap maximum number of traps allowed
- * @param platformPool the pool of objects available for this difficulty
+ * @param height the height of the world for this difficulty level.
+ * @param distance the distance configuration for platform spawning.
+ * @param spawnPool the pool of game objects to spawn for this difficulty level.
+ * @param addOnPool the pool of add-ons to spawn for this difficulty level.
+ * @param platformPool the pool of platforms to spawn for this difficulty level.
  */
 public record Difficult(double height,
         Distance distance,
