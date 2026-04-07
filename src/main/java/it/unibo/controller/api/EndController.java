55d35918e0a740ae9d22e31a3b69a023ca24752a
marco <marcoaresu82@gmail.com>
2026-04-07 21:58:04 +0200

    added java doc

--- src/main/java/it/unibo/controller/api/EndController.java
+++ src/main/java/it/unibo/controller/api/EndController.java
@@ -1,13 +1,34 @@
 package it.unibo.controller.api;
 
+/**
+ * Interface representing the controller for the end screen of the game. It
+ * defines
+ * the methods that the end screen view will call to handle user interactions.
+ */
 public interface EndController {
 
+    /**
+     * Return to the menu.
+     */
     public void menu();
 
+    /**
+     * Restart the game.
+     */
     public void restart();
 
+    /**
+     * Return the score of the player.
+     * 
+     * @return the score of the player.
+     */
     public int getScore();
 
+    /**
+     * Check if the player has achieved a new high score.
+     * 
+     * @return true if the player has achieved a new high score, false otherwise.
+     */
     public boolean isNewHighScore();
 
 }
