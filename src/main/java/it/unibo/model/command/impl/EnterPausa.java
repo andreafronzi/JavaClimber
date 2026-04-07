429e81c0acde4f93f238ada281f347f4b7df8094
marco <marcoaresu82@gmail.com>
2026-04-07 21:59:10 +0200

    added java doc

--- src/main/java/it/unibo/model/command/impl/EnterPausa.java
+++ src/main/java/it/unibo/model/command/impl/EnterPausa.java
@@ -5,11 +5,17 @@ import it.unibo.model.LaunchedGame.impl.PauseState;
 import it.unibo.model.command.api.RunningCommand;
 import it.unibo.model.gameObj.api.Alien;
 
+/**
+ * Rapresent the command to enter in the pause state.
+ */
 public class EnterPausa implements RunningCommand{
 
+    /**
+     * {@inheritDoc} If the command is executed, the game enter in the pause state.
+     */
     @Override
-    public void execute(Alien alien, LaunchedGame launchedGame) {
+    public void execute(final Alien alien, final LaunchedGame launchedGame) {
         launchedGame.setState(new PauseState(launchedGame));
     }
-    
+
 }
