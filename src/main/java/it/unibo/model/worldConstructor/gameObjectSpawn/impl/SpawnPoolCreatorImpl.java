fecf9279a36cff452309c6cc7f9afc7bc00f560d
marco <marcoaresu82@gmail.com>
2026-04-07 22:09:59 +0200

    added java doc

--- src/main/java/it/unibo/model/worldConstructor/gameObjectSpawn/impl/SpawnPoolCreatorImpl.java
+++ src/main/java/it/unibo/model/worldConstructor/gameObjectSpawn/impl/SpawnPoolCreatorImpl.java
@@ -22,11 +22,28 @@ import it.unibo.model.gameObj.api.Platform;
  */
 public class SpawnPoolCreatorImpl implements SpawnPoolCreator {
 
+    /**
+     * Represents a set of a spawn pool that can be used to create game objects
+     * based on spawn chances.
+     */
     private SpawnPool spawnPool;
-    private final BaseWorld world;
-    private final AddOnPositionSetter addOnPositionSetter;
-    // private List<Pair<Double,Function<Vector2d,Trap>>> traps;
 
+    /**
+     * Represents the game world where the created objects will be added.
+     */
+    private final BaseWorld world;
+
+    /**
+     * Responsible for calculating the positions of add-ons (monsters, gadgets,
+     * money) on platforms.
+     */
+    private final AddOnPositionSetter addOnPositionSetter;
+
+    /**
+     * Constructs a SpawnPoolCreatorImpl with the specified game world.
+     *
+     * @param world the game world where the created objects will be added
+     */
     public SpawnPoolCreatorImpl(final BaseWorld world) {
         this.addOnPositionSetter = new AddOnPositionSetterImpl();
         this.world = world;
@@ -35,6 +52,7 @@ public class SpawnPoolCreatorImpl implements SpawnPoolCreator {
     /**
      * {@inheritDoc}
      */
+    @Override
     public void setSpawnPool(final SpawnPool spawnPool) {
         this.spawnPool = spawnPool;
     }
@@ -48,12 +66,18 @@ public class SpawnPoolCreatorImpl implements SpawnPoolCreator {
                 .ifPresent(world::addStaticPlatform);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void createMovingPlatform(final double chance, final Vector2d pos) {
         this.selector(chance, pos.getX(), pos.getY(), this.spawnPool.getMovingPlatformPool())
                 .ifPresent(world::addMovingPlatform);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void createOnTouchPlatform(final double chance, final Vector2d pos) {
         this.selector(chance, pos.getX(), pos.getY(), this.spawnPool.getOnTouchPlatformPool())
@@ -87,20 +111,18 @@ public class SpawnPoolCreatorImpl implements SpawnPoolCreator {
                 .ifPresent(money -> world.addMoney(addOnPositionSetter.generatePosition(money, platform.getWidth())));
     }
 
-    /*
-     * @Override
-     * public Trap createTrap(final double chance, final Vector2d pos) {
-     * for (final var p : this.traps) {
-     * if (chance <= p.getChance()) {
-     * return p.createGameObj(pos);
-     * }
-     * }
-     * return null;
-     * }
+    /**
+     * Selects a game object from the provided pool based on the given spawn chance. 
+     * 
+     * @param <X> the type of game object to be created 
+     * @param chance the spawn chance to compare against the pool probabilities
+     * @param x the x-coordinate for the position of the game object
+     * @param y the y-coordinate for the position of the game object
+     * @param addOns the list of pairs containing spawn probabilities and corresponding game object creation functions
+     * @return an Optional containing the created game object if a match is found, or an empty Optional if no match is found
      */
-
-    public <X extends GameObject> Optional<X> selector(double chance, double x, double y,
-            List<Pair<Double, Function<Vector2d, X>>> addOns) {
+    public <X extends GameObject> Optional<X> selector(final double chance, final double x, final double y,
+            final List<Pair<Double, Function<Vector2d, X>>> addOns) {
         for (final var addOn : addOns) {
             if (chance <= addOn.getX()) {
                 return Optional.of(addOn.getY().apply(new Vector2dImpl(x, y)));
