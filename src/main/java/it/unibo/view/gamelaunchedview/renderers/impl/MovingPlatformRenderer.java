e43c7df79a7e5a604135566f195654f56b42726f
AndreaFronzi <andrea.fronzi@studio.unibo.it>
2026-04-14 23:27:37 +0200

    correct package paths and improve formatting in MovingPlatformRenderer

--- src/main/java/it/unibo/view/gamelaunchedview/renderers/impl/MovingPlatformRenderer.java
+++ src/main/java/it/unibo/view/gamelaunchedview/renderers/impl/MovingPlatformRenderer.java
@@ -1,4 +1,4 @@
-package it.unibo.view.GameLaunchedView.renderers.impl;
+package it.unibo.view.gamelaunchedview.renderers.impl;
 
 import java.util.List;
 import java.util.Objects;
@@ -6,13 +6,16 @@ import java.util.Objects;
 import it.unibo.model.gameobj.api.Platform;
 import it.unibo.view.SpriteEnum;
 import it.unibo.view.SpriteManager;
-import it.unibo.view.GameLaunchedView.renderers.api.EntityRenderer;
+import it.unibo.view.gamelaunchedview.renderers.api.EntityRenderer;
 
-import java.awt.*;
+import java.awt.Graphics2D;
+import java.awt.Image;
 
+/**
+ * Renderer for moving {@link Platform} entities.
+ */
+public final class MovingPlatformRenderer implements EntityRenderer<Platform> {
 
-public class MovingPlatformRenderer implements EntityRenderer<Platform>{
-    
     /**
      * The {@link SpriteManager} used to get the platform sprite.
      */
@@ -32,15 +35,11 @@ public class MovingPlatformRenderer implements EntityRenderer<Platform>{
      */
     @Override
     public void render(final List<Platform> platforms, final Graphics2D g) {
-        final Image sprite = spriteManager.get(SpriteEnum.HORIZONTAL_MOVE_PLATFORM);
-        if (!Objects.isNull(sprite)) {
+        final Image sprite = this.spriteManager.get(SpriteEnum.HORIZONTAL_MOVE_PLATFORM);
+        if (Objects.nonNull(sprite)) {
             platforms.forEach(platform -> {
-                g.drawImage(sprite,
-                        (int) platform.getPosX(),
-                        (int) platform.getPosY(),
-                        (int) platform.getWidth(),
-                        (int) platform.getHeight(),
-                        null);
+                g.drawImage(sprite, (int) platform.getPosX(), (int) platform.getPosY(), (int) platform.getWidth(),
+                        (int) platform.getHeight(), null);
             });
         }
     }
