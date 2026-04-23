d8c60e15a8b703ad92a319fa2f07cb75ffc2c973
marco <marcoaresu82@gmail.com>
2026-04-23 11:46:02 +0200

    added suppression for warnings

--- src/main/java/it/unibo/model/worldconstructor/worldgenerator/api/WorldDifficult.java
+++ src/main/java/it/unibo/model/worldconstructor/worldgenerator/api/WorldDifficult.java
@@ -4,7 +4,7 @@ package it.unibo.model.worldconstructor.worldgenerator.api;
  * Interface for managing world difficulty.
  * Responsible for adjusting the game difficulty based on player progression.
  */
-public interface WorldDifficult {
+public interface WorldDifficult { // NOPMD this interface is necessary to define the contract for difficulty management.
 
     /**
      * Checks and updates the current difficulty level of the world.
