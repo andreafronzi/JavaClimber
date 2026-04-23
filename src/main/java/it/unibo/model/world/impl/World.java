b23656ee9a110bba6ca7cd80cbc28c3d25783aa6
marco <marcoaresu82@gmail.com>
2026-04-23 23:42:12 +0200

    added suppression for warnings

--- src/main/java/it/unibo/model/world/impl/World.java
+++ src/main/java/it/unibo/model/world/impl/World.java
@@ -1,11 +1,13 @@
 package it.unibo.model.world.impl;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import it.unibo.model.world.api.GameWorld;
 import it.unibo.model.world.api.QueueWorld;
 
 /**
  * Class representing the complete game world.
- * It combines an "upper world" (queue of upcoming entities) and a "real world" (active entities).
+ * It combines an "upper world" (queue of upcoming entities) and a "real world"
+ * (active entities).
  */
 public class World {
 
@@ -23,8 +25,10 @@ public class World {
      * Constructs a new World.
      * 
      * @param upperWorld the queue-based world for upcoming entities
-     * @param realWorld the active game world
+     * @param realWorld  the active game world
      */
+    @SuppressFBWarnings(value = "EI_EXPOSE_REP2", justification = "The upperWorld and realWorld are mutable objects, "
+            + "the world store the two world")
     public World(final QueueWorld upperWorld, final GameWorld realWorld) {
         this.upperWorld = upperWorld;
         this.realWorld = realWorld;
@@ -35,6 +39,8 @@ public class World {
      * 
      * @return the upper world
      */
+    @SuppressFBWarnings(value = "EI_EXPOSE_REP", justification = "The getter is necessary to access the upper world,"
+            + " which is a mutable object, but it is necessary to pass to use it.")
     public QueueWorld getUpperWorld() {
         return this.upperWorld;
     }
@@ -44,6 +50,8 @@ public class World {
      * 
      * @return the real world
      */
+    @SuppressFBWarnings(value = "EI_EXPOSE_REP", justification = "The getter is necessary to access the real world, which is"
+            + "a mutable object, but it is necessary to pass to use it.")
     public GameWorld getRealWorld() {
         return this.realWorld;
     }
